84164f38cb81aeb19de74fc10d366236747bde18
Krovcia <143706464+Krovcia@users.noreply.github.com>
2023-10-06 21:46:18 +0700

    Update Kovi.lua

--- Kovi.lua
+++ Kovi.lua
@@ -1,1 +1,61 @@
+wait (10)    
+local args = {[1]="MORIOH"}
+        game:GetService("ReplicatedStorage"):WaitForChild("endpoints"):WaitForChild("client_to_server"):WaitForChild("redeem_code"):InvokeServer(unpack(args))
+        wait()
+        local args = {[1]="HOLYGRAIL"}
+        game:GetService("ReplicatedStorage"):WaitForChild("endpoints"):WaitForChild("client_to_server"):WaitForChild("redeem_code"):InvokeServer(unpack(args))
+        wait()
+        local args = {[1]="EventClover",[2]="gems10"}
+        game:GetService("ReplicatedStorage"):WaitForChild("endpoints"):WaitForChild("client_to_server"):WaitForChild("buy_from_banner"):InvokeServer(unpack(args))
+        wait(3)
+        local args = {[1]="EventClover",[2]="gems10"}
+        game:GetService("ReplicatedStorage"):WaitForChild("endpoints"):WaitForChild("client_to_server"):WaitForChild("buy_from_banner"):InvokeServer(unpack(args))
+wait()
+getgenv().SecureMode = true
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
+ 
+local MainWindow = Rayfield:CreateWindow({
+    Name = "ALVINO",
+    LoadingTitle = "Sabar...",
+    LoadingSubtitle = "by KONTOL",
+    ConfigurationSaving = {
+       Enabled = false,
+       FolderName = nil, -- Create a custom folder for your hub/game
+       FileName = "McDonalds Hub"
+    }
+ })
 
+local MainTab = MainWindow:CreateTab("PELER", 7053923059) -- Title, Image
+
+local Button = MainTab:CreateButton({
+    Name = "============= < 10TIMES =============",
+    Callback = function(v)
+
+        local args = {[1]="EventClover",[2]="gems1"}
+        game:GetService("ReplicatedStorage"):WaitForChild("endpoints"):WaitForChild("client_to_server"):WaitForChild("buy_from_banner"):InvokeServer(unpack(args))
+        wait(3)
+        local args = {[1]="EventClover",[2]="gems1"}
+        game:GetService("ReplicatedStorage"):WaitForChild("endpoints"):WaitForChild("client_to_server"):WaitForChild("buy_from_banner"):InvokeServer(unpack(args))
+        wait(3)
+        local args = {[1]="EventClover",[2]="gems1"}
+        game:GetService("ReplicatedStorage"):WaitForChild("endpoints"):WaitForChild("client_to_server"):WaitForChild("buy_from_banner"):InvokeServer(unpack(args))
+        wait(3)
+        local args = {[1]="EventClover",[2]="gems1"}
+        game:GetService("ReplicatedStorage"):WaitForChild("endpoints"):WaitForChild("client_to_server"):WaitForChild("buy_from_banner"):InvokeServer(unpack(args))
+        wait(3)
+        local args = {[1]="EventClover",[2]="gems1"}
+        game:GetService("ReplicatedStorage"):WaitForChild("endpoints"):WaitForChild("client_to_server"):WaitForChild("buy_from_banner"):InvokeServer(unpack(args))
+        wait(3)
+        local args = {[1]="EventClover",[2]="gems1"}
+        game:GetService("ReplicatedStorage"):WaitForChild("endpoints"):WaitForChild("client_to_server"):WaitForChild("buy_from_banner"):InvokeServer(unpack(args))
+        wait(3)
+        local args = {[1]="EventClover",[2]="gems1"}
+        game:GetService("ReplicatedStorage"):WaitForChild("endpoints"):WaitForChild("client_to_server"):WaitForChild("buy_from_banner"):InvokeServer(unpack(args))
+        wait(3)
+        local args = {[1]="EventClover",[2]="gems1"}
+        game:GetService("ReplicatedStorage"):WaitForChild("endpoints"):WaitForChild("client_to_server"):WaitForChild("buy_from_banner"):InvokeServer(unpack(args))
+        wait(3)
+        local args = {[1]="EventClover",[2]="gems1"}
+        game:GetService("ReplicatedStorage"):WaitForChild("endpoints"):WaitForChild("client_to_server"):WaitForChild("buy_from_banner"):InvokeServer(unpack(args))
+    end,
+})
