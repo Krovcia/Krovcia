1238bf553cdd2d1df34904040f41b5142b641a54
Krovcia <143706464+Krovcia@users.noreply.github.com>
2023-11-22 01:54:02 +0700

    Update kord.lua

--- kord.lua
+++ kord.lua
@@ -1,55 +1,96 @@
-repeat wait() until game:IsLoaded()
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
-local Window = Rayfield:CreateWindow({
-   Name = "Rayfield Example Window",
-   LoadingTitle = "Rayfield Interface Suite",
-   LoadingSubtitle = "by Sirius",
-   ConfigurationSaving = {
-      Enabled = true,
-      FolderName = nil, -- Create a custom folder for your hub/game
-      FileName = "Big Hub"
-   }
-})
-local Tab = Window:CreateTab("Tab Example", 4483362458) -- Title, Image
 
-local Toggle = Tab:CreateToggle({
-   Name = "Leopard",
-   CurrentValue = false,
-   Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-   Callback = function(Value)
-repeat
+wait ()
+
 local args={[1] = "Leopard",[2] = "AfterimageCombo",[3] = {}}game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
-wait(2)
-local args={[1] = "Leopard",[2] = "SpiralKick",[3] = {}}game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
-wait(2)
-local args={[1] = "Leopard",[2] = "PredatorsProwl",[3] = {}}
-wait(2)
-local args={[1] = "Leopard",[2] = "ClawBarrage",[3] = {}}game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
-until false
-   end,
-})
-
-local Toggle = Tab:CreateToggle({
-   Name = "Ope",
-   CurrentValue = false,
-   Flag = "Toggle2", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-   Callback = function(Value)
-repeat
+wait()
 local args={[1] = "Ope",[2] = "Room",[3] = {}}game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
+wait()
+local args={[1] = "Dough",[2] = "GrilledDough",[3] = {}}game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
+wait()
+local args = {[1] = "Venom",[2] = "ChloroFrenzy",[3] = {["MouseRay"] = {["Normal"] = Vector3.xAxis,["Direction"] = Vector3.new(-247.51364135742188, -120.57630920410156, 119.15691375732422),
+["Origin"] = Vector3.new(75.88587188720703, 813.279296875, 921.138916015625),["Instance"] = workspace:WaitForChild("Map"):WaitForChild("Part"),["Distance"] = 114.6936264038086,["Material"] = Enum.Material.Brick,
+["Position"] = Vector3.new(-18.7415771484375, 767.1815185546875, 966.6940307617188)}}}game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
+wait()
+local args={[1] = "DragonV2",[2] = "ThunderBagua",[3] = {}}game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
+wait()
+local args={[1] = "Lightning",[2] = "VoltageUp",[3] = {}}game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
+
 wait(2)
+
+local args={[1] = "Leopard",[2] = "SpiralKick",[3] = {}}game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
+wait()
 local args={[1] = "Ope",[2] = "Takt",[3] = {}}game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
+wait()
+local args={[1] = "Dough",[2] = "Muddle",[3] = {}}game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
+wait()
+local args={[1] = "Venom",[2] = "VenomRoad",[3] = {}}game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
+wait()
+local args = {[1] = "DragonV2",[2] = "InfernoBreath",[3] = {["MouseRay"] = {["Normal"] = Vector3.new(0.5000314116477966, 0, 0.8660072684288025),
+["Direction"] = Vector3.new(-132.1378631591797, -46.01848220825195, -142.90518188476562),["Origin"] = Vector3.new(727.1622924804688, 741.6847534179688, 1061.2464599609375),
+["Instance"] = workspace:WaitForChild("Map"):WaitForChild("Model"):WaitForChild("Part"),["Distance"] = 37.810218811035156,["Material"] = Enum.Material.Brick,
+["Position"] = Vector3.new(702.1814575195312, 732.9849243164062, 1034.2301025390625)}}}game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
+wait()
+local args={[1] = "Lightning",[2] = "LightningPalm",[3] = {}}game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
+
+
 wait(2)
+
+local args={[1] = "Leopard",[2] = "PredatorsProwl",[3] = {["MouseRay"] = {["Normal"] = Vector3.yAxis,
+["Direction"] = Vector3.new(136.6070098876953, -166.79452514648438, 208.60997009277344),["Origin"] = Vector3.new(422.1204833984375, 741.421142578125, -643.9622192382812),
+["Instance"] = workspace:WaitForChild("Map"):WaitForChild("Model"):WaitForChild("Part"),["Distance"] = 20.08648681640625,["Material"] = Enum.Material.Foil,
+["Position"] = Vector3.new(431.2669982910156, 730.25341796875, -629.9947509765625)}}}game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
+wait()
 local args={[1] = "Ope",[2] = "Wreckage",[3] = {}}game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
+wait()
+local args={[1] = "Dough",[2] = "LotusDoughCombo",[3] = {}}game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
+wait()
+local args={[1] = "Venom",[2] = "ToxicHydra",[3] = {}}game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
+wait()
+local args={[1] = "DragonV2",[2] = "DragonTwister",[3] = {}}game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
+wait()
+local args={[1] = "Lightning",[2] = "CrashingThunder",[3] = {}}game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
+
+
 wait(2)
+
+local args={[1] = "Leopard",[2] = "ClawBarrage",[3] = {}}game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
+wait()
 local args={[1] = "Ope",[2] = "GammaKnife",[3] = {}}game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
+wait()
+local args={[1] = "Dough",[2] = "Chesnut",[3] = {}}game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
+wait()
+local args={[1] = "Venom",[2] = "PoisonStorm",[3] = {}}game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
+wait()
+local args={[1] = "DragonV2",[2] = "BeastsBellow",[3] = {}}game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
+wait()
+local args={[1] = "Lightning",[2] = "ProjectedBurst",[3] = {}}game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
+
+
 wait(2)
+
 local args={[1] = "Ope",[2] = "HurricaneShock",[3] = {}}game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
+wait()
+local args={[1] = "Dough",[2] = "Anemone",[3] = {}}game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
+wait()
+local args={[1] = "Venom",[2] = "HydraCyclone",[3] = {}}game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
+wait()
+local args={[1] = "Lightning",[2] = "CrushingJudgment",[3] = {}}game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
+
 wait(2)
+
 local args={[1] = "Ope",[2] = "Mes",[3] = {}}game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
-wait(2)
-until false
-   end,
-})
+wait()
+local args = {[1] = "Dough",[2] = "Buzzcut",[3] = {["MouseRay"] = {["Normal"] = Vector3.yAxis,
+["Direction"] = Vector3.new(265.23370361328125, -67.58796691894531, 122.8126449584961),["Origin"] = Vector3.new(-837.7066650390625, 783.6351928710938, -796.7230224609375),
+["Instance"] = workspace:WaitForChild("Map"):WaitForChild("Part"),["Distance"] = 31.973045349121094,["Material"] = Enum.Material.Grass,
+["Position"] = Vector3.new(-809.4389038085938, 776.431884765625, -783.634033203125)}}}game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
+wait()
+local args={[1] = "Lightning",[2] = "Raigo",[3] = {}}game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
+
+
+
+
+
 
 
 
