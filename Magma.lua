26cb03d560001702a9f49a88b34f4e3496d53589
Krovcia <143706464+Krovcia@users.noreply.github.com>
2024-01-16 18:14:55 +0700

    Update Magma.lua

--- Magma.lua
+++ Magma.lua
@@ -1,21 +1,22 @@
 repeat
 keypress(0x31)
+local VirtualInputManager = game:GetService("VirtualInputManager")
+VirtualInputManager:SendMouseButtonEvent(480, 20, 0, true, game, 1)
 wait(1)
 keyrelease(0x31)
-local VirtualInputManager = game:GetService("VirtualInputManager")
-VirtualInputManager:SendMouseButtonEvent(480, 20, 0, true, game, 1)
 
 keypress(0x32)
+local VirtualInputManager = game:GetService("VirtualInputManager")
+VirtualInputManager:SendMouseButtonEvent(480, 20, 0, true, game, 1)
 wait(1)
 keyrelease(0x32)
-local VirtualInputManager = game:GetService("VirtualInputManager")
-VirtualInputManager:SendMouseButtonEvent(480, 20, 0, true, game, 1)
+
 
 keypress(0x33)
-wait(1)
-keyrelease(0x33)
 local VirtualInputManager = game:GetService("VirtualInputManager")
 VirtualInputManager:SendMouseButtonEvent(480, 20, 0, true, game, 1)
+wait(1)
+keyrelease(0x33)
 
 wait(1)
-until(false))
+until(false)
