431e18b58a426b9ee7a7b73e6853af3d80d5b6ec
Krovcia <143706464+Krovcia@users.noreply.github.com>
2024-01-16 18:08:45 +0700

    Update Magma.lua

--- Magma.lua
+++ Magma.lua
@@ -1,37 +1,21 @@
 repeat
-local args = {[1] = "MagmaV2",[2] = "MagmaFloor",[3] = {}}
-game:GetService("ReplicatedStorage"):WaitForChild("Replicator"):InvokeServer(unpack(args))
-wait(2)
-local args = {[1] = "MagmaV2",[2] = "Meigo",[3] = {}}
-game:GetService("ReplicatedStorage"):WaitForChild("Replicator"):InvokeServer(unpack(args))
-wait(2)
-local args = {[1] = "MagmaV2",[2] = "MagmaFloor",[3] = {}}
-game:GetService("ReplicatedStorage"):WaitForChild("Replicator"):InvokeServer(unpack(args))
-wait(2)
-local args = {[1] = "MagmaV2",[2] = "Meigo",[3] = {}}
-game:GetService("ReplicatedStorage"):WaitForChild("Replicator"):InvokeServer(unpack(args))
-wait(2)
-local args = {[1] = "MagmaV2",[2] = "MagmaFloor",[3] = {}}
-game:GetService("ReplicatedStorage"):WaitForChild("Replicator"):InvokeServer(unpack(args))
-wait(2)
-local args = {[1] = "MagmaV2",[2] = "Meigo",[3] = {}}
-game:GetService("ReplicatedStorage"):WaitForChild("Replicator"):InvokeServer(unpack(args))
-wait(2)
-local args = {[1] = "MagmaV2",[2] = "MagmaFloor",[3] = {}}
-game:GetService("ReplicatedStorage"):WaitForChild("Replicator"):InvokeServer(unpack(args))
-wait(2)
-local args = {[1] = "MagmaV2",[2] = "Meigo",[3] = {}}
-game:GetService("ReplicatedStorage"):WaitForChild("Replicator"):InvokeServer(unpack(args))
-wait(2)
-local args = {[1] = "MagmaV2",[2] = "MagmaFloor",[3] = {}}
-game:GetService("ReplicatedStorage"):WaitForChild("Replicator"):InvokeServer(unpack(args))
-wait(2)
-local args = {[1] = "MagmaV2",[2] = "Meigo",[3] = {}}
-game:GetService("ReplicatedStorage"):WaitForChild("Replicator"):InvokeServer(unpack(args))
-wait(2)
-local args = {[1] = "MagmaV2",[2] = "MagmaFloor",[3] = {}}
-game:GetService("ReplicatedStorage"):WaitForChild("Replicator"):InvokeServer(unpack(args))
-wait(2)
-local args = {[1] = "MagmaV2",[2] = "Meigo",[3] = {}}
-game:GetService("ReplicatedStorage"):WaitForChild("Replicator"):InvokeServer(unpack(args))
-wait(2)
+keypress(0x31)
+wait(1)
+keyrelease(0x31)
+local VirtualInputManager = game:GetService("VirtualInputManager")
+VirtualInputManager:SendMouseButtonEvent(480, 20, 0, true, game, 1)
+
+keypress(0x32)
+wait(1)
+keyrelease(0x32)
+local VirtualInputManager = game:GetService("VirtualInputManager")
+VirtualInputManager:SendMouseButtonEvent(480, 20, 0, true, game, 1)
+
+keypress(0x33)
+wait(1)
+keyrelease(0x33)
+local VirtualInputManager = game:GetService("VirtualInputManager")
+VirtualInputManager:SendMouseButtonEvent(480, 20, 0, true, game, 1)
+
+wait(1)
+until(false))
