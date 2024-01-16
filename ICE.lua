5560a41f6df724c52c4b2170971c0568d2df63ed
Krovcia <143706464+Krovcia@users.noreply.github.com>
2024-01-16 11:16:09 +0700

    Update ICE.lua

--- ICE.lua
+++ ICE.lua
@@ -14,19 +14,13 @@ wait(2)
 local args = {[1] = "IceV2",[2] = "BlizzardBlade",[3] = {}}
 game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
 wait(2)
-local args = {[1] = "IceV2",[2] = "PiercingGlacier",[3] = {
-["MouseRay"] = {
-["Normal"] = Vector3.new(-0.02799174003303051, 0.9992106556892395, 0.028183991089463234),
-["Direction"] = Vector3.new(-4.379700183868408, -82.0096664428711, -182.3601837158203),
-["Origin"] = Vector3.new(407.349853515625, 747.2974853515625, -660.3728637695312),
-["Instance"] = workspace:WaitForChild("SafeZones"):WaitForChild("Dressrosa"):WaitForChild("MainColor"),
-["Distance"] = 29.648469924926758,
-["Material"] = Enum.Material.Slate,
-["Position"] = Vector3.new(406.7005920410156, 735.1401977539062, -687.4063720703125)
-}}}
-game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
+local args = {[1] = "IceV2",[2] = "IceHail",[3] = {["MouseRay"] = {
+["Normal"] = Vector3.yAxis,["Direction"] = Vector3.new(108.53388214111328, -90.6153793334961, -141.4540557861328),
+["Origin"] = Vector3.new(898.65234375, 755.7377319335938, 1204.8072509765625),["Instance"] = workspace:WaitForChild("SafeZones"):WaitForChild("Colloseum2"):WaitForChild("MainColor"),
+["Distance"] = 45.62202453613281,["Material"] = Enum.Material.Slate,["Position"] = Vector3.new(923.4100341796875, 735.0674438476562, 1172.5401611328125)
+}}}game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
 wait(2)
-local args = {[1] = "IceV2",[2] = "BlizzardBlade",[3] = {}}
+local args = {[1] = "IceV2",[2] = "AbsoluteZero",[3] = {}}
 game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
 wait(2)
 local args = {[1] = "IceV2",[2] = "PiercingGlacier",[3] = {
@@ -44,19 +38,13 @@ wait(2)
 local args = {[1] = "IceV2",[2] = "BlizzardBlade",[3] = {}}
 game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
 wait(2)
-local args = {[1] = "IceV2",[2] = "PiercingGlacier",[3] = {
-["MouseRay"] = {
-["Normal"] = Vector3.new(-0.02799174003303051, 0.9992106556892395, 0.028183991089463234),
-["Direction"] = Vector3.new(-4.379700183868408, -82.0096664428711, -182.3601837158203),
-["Origin"] = Vector3.new(407.349853515625, 747.2974853515625, -660.3728637695312),
-["Instance"] = workspace:WaitForChild("SafeZones"):WaitForChild("Dressrosa"):WaitForChild("MainColor"),
-["Distance"] = 29.648469924926758,
-["Material"] = Enum.Material.Slate,
-["Position"] = Vector3.new(406.7005920410156, 735.1401977539062, -687.4063720703125)
-}}}
-game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
+local args = {[1] = "IceV2",[2] = "IceHail",[3] = {["MouseRay"] = {
+["Normal"] = Vector3.yAxis,["Direction"] = Vector3.new(108.53388214111328, -90.6153793334961, -141.4540557861328),
+["Origin"] = Vector3.new(898.65234375, 755.7377319335938, 1204.8072509765625),["Instance"] = workspace:WaitForChild("SafeZones"):WaitForChild("Colloseum2"):WaitForChild("MainColor"),
+["Distance"] = 45.62202453613281,["Material"] = Enum.Material.Slate,["Position"] = Vector3.new(923.4100341796875, 735.0674438476562, 1172.5401611328125)
+}}}game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
 wait(2)
-local args = {[1] = "IceV2",[2] = "BlizzardBlade",[3] = {}}
+local args = {[1] = "IceV2",[2] = "AbsoluteZero",[3] = {}}
 game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
 wait(2)
 local args = {[1] = "IceV2",[2] = "PiercingGlacier",[3] = {
@@ -74,6 +62,15 @@ wait(2)
 local args = {[1] = "IceV2",[2] = "BlizzardBlade",[3] = {}}
 game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
 wait(2)
+local args = {[1] = "IceV2",[2] = "IceHail",[3] = {["MouseRay"] = {
+["Normal"] = Vector3.yAxis,["Direction"] = Vector3.new(108.53388214111328, -90.6153793334961, -141.4540557861328),
+["Origin"] = Vector3.new(898.65234375, 755.7377319335938, 1204.8072509765625),["Instance"] = workspace:WaitForChild("SafeZones"):WaitForChild("Colloseum2"):WaitForChild("MainColor"),
+["Distance"] = 45.62202453613281,["Material"] = Enum.Material.Slate,["Position"] = Vector3.new(923.4100341796875, 735.0674438476562, 1172.5401611328125)
+}}}game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
+wait(2)
+local args = {[1] = "IceV2",[2] = "AbsoluteZero",[3] = {}}
+game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
+wait(2)
 local args = {[1] = "IceV2",[2] = "PiercingGlacier",[3] = {
 ["MouseRay"] = {
 ["Normal"] = Vector3.new(-0.02799174003303051, 0.9992106556892395, 0.028183991089463234),
@@ -87,5 +84,38 @@ local args = {[1] = "IceV2",[2] = "PiercingGlacier",[3] = {
 game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
 wait(2)
 local args = {[1] = "IceV2",[2] = "BlizzardBlade",[3] = {}}
+game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
+wait(2)
+local args = {[1] = "IceV2",[2] = "IceHail",[3] = {["MouseRay"] = {
+["Normal"] = Vector3.yAxis,["Direction"] = Vector3.new(108.53388214111328, -90.6153793334961, -141.4540557861328),
+["Origin"] = Vector3.new(898.65234375, 755.7377319335938, 1204.8072509765625),["Instance"] = workspace:WaitForChild("SafeZones"):WaitForChild("Colloseum2"):WaitForChild("MainColor"),
+["Distance"] = 45.62202453613281,["Material"] = Enum.Material.Slate,["Position"] = Vector3.new(923.4100341796875, 735.0674438476562, 1172.5401611328125)
+}}}game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
+wait(2)
+local args = {[1] = "IceV2",[2] = "AbsoluteZero",[3] = {}}
+game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
+wait(2)
+local args = {[1] = "IceV2",[2] = "PiercingGlacier",[3] = {
+["MouseRay"] = {
+["Normal"] = Vector3.new(-0.02799174003303051, 0.9992106556892395, 0.028183991089463234),
+["Direction"] = Vector3.new(-4.379700183868408, -82.0096664428711, -182.3601837158203),
+["Origin"] = Vector3.new(407.349853515625, 747.2974853515625, -660.3728637695312),
+["Instance"] = workspace:WaitForChild("SafeZones"):WaitForChild("Dressrosa"):WaitForChild("MainColor"),
+["Distance"] = 29.648469924926758,
+["Material"] = Enum.Material.Slate,
+["Position"] = Vector3.new(406.7005920410156, 735.1401977539062, -687.4063720703125)
+}}}
+game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
+wait(2)
+local args = {[1] = "IceV2",[2] = "BlizzardBlade",[3] = {}}
+game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
+wait(2)
+local args = {[1] = "IceV2",[2] = "IceHail",[3] = {["MouseRay"] = {
+["Normal"] = Vector3.yAxis,["Direction"] = Vector3.new(108.53388214111328, -90.6153793334961, -141.4540557861328),
+["Origin"] = Vector3.new(898.65234375, 755.7377319335938, 1204.8072509765625),["Instance"] = workspace:WaitForChild("SafeZones"):WaitForChild("Colloseum2"):WaitForChild("MainColor"),
+["Distance"] = 45.62202453613281,["Material"] = Enum.Material.Slate,["Position"] = Vector3.new(923.4100341796875, 735.0674438476562, 1172.5401611328125)
+}}}game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
+wait(2)
+local args = {[1] = "IceV2",[2] = "AbsoluteZero",[3] = {}}
 game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
 wait(2)
