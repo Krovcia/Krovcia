d9c971a866607c864e9d67de4b6b618b9e9c92e4
Krovcia <143706464+Krovcia@users.noreply.github.com>
2024-01-16 12:15:15 +0700

    Update ICE.lua

--- ICE.lua
+++ ICE.lua
@@ -1,4 +1,5 @@
 wait(90)
+repeat
 local args = {[1] = "IceV2",[2] = "PiercingGlacier",[3] = {
 ["MouseRay"] = {
 ["Normal"] = Vector3.new(-0.02799174003303051, 0.9992106556892395, 0.028183991089463234),
@@ -119,3 +120,4 @@ wait(2)
 local args = {[1] = "IceV2",[2] = "AbsoluteZero",[3] = {}}
 game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
 wait(2)
+until(false)
