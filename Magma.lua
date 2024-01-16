b05af305842bbf5a831598ab357ff2d34c818e92
Krovcia <143706464+Krovcia@users.noreply.github.com>
2024-01-16 11:11:51 +0700

    Update Magma.lua

--- Magma.lua
+++ Magma.lua
@@ -26,31 +26,10 @@ wait(2)
 local args = {[1] = "MagmaV2",[2] = "MagmaFloor",[3] = {}}
 game:GetService("ReplicatedStorage"):WaitForChild("Replicator"):InvokeServer(unpack(args))
 wait(2)
-local args = {[1] = "MagmaV2",[2] = "HoundBlaze",[3] = {
-["MouseRay"] = {
-["Normal"] = Vector3.new(0.00154804065823555, 0.9999935030937195, 0.0032721238676458597),
-["Direction"] = Vector3.new(184.97593688964844, -146.860107421875, 184.97573852539062),
-["Origin"] = Vector3.new(-112.73625183105469, 1056.1458740234375, 238.81497192382812),
-["Instance"] = workspace:WaitForChild("Map"):WaitForChild("collosalstructure"):WaitForChild("Cube"),
-["Distance"] = 50.767051696777344,
-["Material"] = Enum.Material.Brick,
-["Position"] = Vector3.new(-81.43397521972656, 1031.293701171875, 270.1172180175781)
-}}}
+local args = {[1] = "MagmaV2",[2] = "Meigo",[3] = {}}
 game:GetService("ReplicatedStorage"):WaitForChild("Replicator"):InvokeServer(unpack(args))
 wait(2)
-local args = {[1] = "MagmaV2",[2] = "MagmaRain",[3] = {
-["MouseRay"] = {
-["Normal"] = Vector3.yAxis,
-["Direction"] = Vector3.new(-232.41555786132812, -179.79122924804688, -60.48246383666992),
-["Origin"] = Vector3.new(423.5809020996094, 763.4757080078125, -674.6029052734375),
-["Instance"] = workspace:WaitForChild("SafeZones"):WaitForChild("Dressrosa"):WaitForChild("MainColor"),
-["Distance"] = 50.88035583496094,
-["Material"] = Enum.Material.Slate,
-["Position"] = Vector3.new(384.1629333496094, 732.98291015625, -684.8607788085938)
-}}}
-game:GetService("ReplicatedStorage"):WaitForChild("Replicator"):InvokeServer(unpack(args))
-wait(2)
-local args = {[1] = "MagmaV2",[2] = "MagmaFloor",[3] = {}}
+local args = {[1] = "MagmaV2",[2] = "CrimsonHowl",[3] = {}}
 game:GetService("ReplicatedStorage"):WaitForChild("Replicator"):InvokeServer(unpack(args))
 wait(2)
 local args = {[1] = "MagmaV2",[2] = "HoundBlaze",[3] = {
@@ -80,31 +59,10 @@ wait(2)
 local args = {[1] = "MagmaV2",[2] = "MagmaFloor",[3] = {}}
 game:GetService("ReplicatedStorage"):WaitForChild("Replicator"):InvokeServer(unpack(args))
 wait(2)
-local args = {[1] = "MagmaV2",[2] = "HoundBlaze",[3] = {
-["MouseRay"] = {
-["Normal"] = Vector3.new(0.00154804065823555, 0.9999935030937195, 0.0032721238676458597),
-["Direction"] = Vector3.new(184.97593688964844, -146.860107421875, 184.97573852539062),
-["Origin"] = Vector3.new(-112.73625183105469, 1056.1458740234375, 238.81497192382812),
-["Instance"] = workspace:WaitForChild("Map"):WaitForChild("collosalstructure"):WaitForChild("Cube"),
-["Distance"] = 50.767051696777344,
-["Material"] = Enum.Material.Brick,
-["Position"] = Vector3.new(-81.43397521972656, 1031.293701171875, 270.1172180175781)
-}}}
+local args = {[1] = "MagmaV2",[2] = "Meigo",[3] = {}}
 game:GetService("ReplicatedStorage"):WaitForChild("Replicator"):InvokeServer(unpack(args))
 wait(2)
-local args = {[1] = "MagmaV2",[2] = "MagmaRain",[3] = {
-["MouseRay"] = {
-["Normal"] = Vector3.yAxis,
-["Direction"] = Vector3.new(-232.41555786132812, -179.79122924804688, -60.48246383666992),
-["Origin"] = Vector3.new(423.5809020996094, 763.4757080078125, -674.6029052734375),
-["Instance"] = workspace:WaitForChild("SafeZones"):WaitForChild("Dressrosa"):WaitForChild("MainColor"),
-["Distance"] = 50.88035583496094,
-["Material"] = Enum.Material.Slate,
-["Position"] = Vector3.new(384.1629333496094, 732.98291015625, -684.8607788085938)
-}}}
-game:GetService("ReplicatedStorage"):WaitForChild("Replicator"):InvokeServer(unpack(args))
-wait(2)
-local args = {[1] = "MagmaV2",[2] = "MagmaFloor",[3] = {}}
+local args = {[1] = "MagmaV2",[2] = "CrimsonHowl",[3] = {}}
 game:GetService("ReplicatedStorage"):WaitForChild("Replicator"):InvokeServer(unpack(args))
 wait(2)
 local args = {[1] = "MagmaV2",[2] = "HoundBlaze",[3] = {
@@ -134,6 +92,12 @@ wait(2)
 local args = {[1] = "MagmaV2",[2] = "MagmaFloor",[3] = {}}
 game:GetService("ReplicatedStorage"):WaitForChild("Replicator"):InvokeServer(unpack(args))
 wait(2)
+local args = {[1] = "MagmaV2",[2] = "Meigo",[3] = {}}
+game:GetService("ReplicatedStorage"):WaitForChild("Replicator"):InvokeServer(unpack(args))
+wait(2)
+local args = {[1] = "MagmaV2",[2] = "CrimsonHowl",[3] = {}}
+game:GetService("ReplicatedStorage"):WaitForChild("Replicator"):InvokeServer(unpack(args))
+wait(2)
 local args = {[1] = "MagmaV2",[2] = "HoundBlaze",[3] = {
 ["MouseRay"] = {
 ["Normal"] = Vector3.new(0.00154804065823555, 0.9999935030937195, 0.0032721238676458597),
@@ -159,5 +123,44 @@ local args = {[1] = "MagmaV2",[2] = "MagmaRain",[3] = {
 game:GetService("ReplicatedStorage"):WaitForChild("Replicator"):InvokeServer(unpack(args))
 wait(2)
 local args = {[1] = "MagmaV2",[2] = "MagmaFloor",[3] = {}}
+game:GetService("ReplicatedStorage"):WaitForChild("Replicator"):InvokeServer(unpack(args))
+wait(2)
+local args = {[1] = "MagmaV2",[2] = "Meigo",[3] = {}}
+game:GetService("ReplicatedStorage"):WaitForChild("Replicator"):InvokeServer(unpack(args))
+wait(2)
+local args = {[1] = "MagmaV2",[2] = "CrimsonHowl",[3] = {}}
+game:GetService("ReplicatedStorage"):WaitForChild("Replicator"):InvokeServer(unpack(args))
+wait(2)
+local args = {[1] = "MagmaV2",[2] = "HoundBlaze",[3] = {
+["MouseRay"] = {
+["Normal"] = Vector3.new(0.00154804065823555, 0.9999935030937195, 0.0032721238676458597),
+["Direction"] = Vector3.new(184.97593688964844, -146.860107421875, 184.97573852539062),
+["Origin"] = Vector3.new(-112.73625183105469, 1056.1458740234375, 238.81497192382812),
+["Instance"] = workspace:WaitForChild("Map"):WaitForChild("collosalstructure"):WaitForChild("Cube"),
+["Distance"] = 50.767051696777344,
+["Material"] = Enum.Material.Brick,
+["Position"] = Vector3.new(-81.43397521972656, 1031.293701171875, 270.1172180175781)
+}}}
+game:GetService("ReplicatedStorage"):WaitForChild("Replicator"):InvokeServer(unpack(args))
+wait(2)
+local args = {[1] = "MagmaV2",[2] = "MagmaRain",[3] = {
+["MouseRay"] = {
+["Normal"] = Vector3.yAxis,
+["Direction"] = Vector3.new(-232.41555786132812, -179.79122924804688, -60.48246383666992),
+["Origin"] = Vector3.new(423.5809020996094, 763.4757080078125, -674.6029052734375),
+["Instance"] = workspace:WaitForChild("SafeZones"):WaitForChild("Dressrosa"):WaitForChild("MainColor"),
+["Distance"] = 50.88035583496094,
+["Material"] = Enum.Material.Slate,
+["Position"] = Vector3.new(384.1629333496094, 732.98291015625, -684.8607788085938)
+}}}
+game:GetService("ReplicatedStorage"):WaitForChild("Replicator"):InvokeServer(unpack(args))
+wait(2)
+local args = {[1] = "MagmaV2",[2] = "MagmaFloor",[3] = {}}
+game:GetService("ReplicatedStorage"):WaitForChild("Replicator"):InvokeServer(unpack(args))
+wait(2)
+local args = {[1] = "MagmaV2",[2] = "Meigo",[3] = {}}
+game:GetService("ReplicatedStorage"):WaitForChild("Replicator"):InvokeServer(unpack(args))
+wait(2)
+local args = {[1] = "MagmaV2",[2] = "CrimsonHowl",[3] = {}}
 game:GetService("ReplicatedStorage"):WaitForChild("Replicator"):InvokeServer(unpack(args))
 wait(2)
