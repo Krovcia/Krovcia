631b034e5bdda1872c26f4e183911cd477db9a40
Krovcia <143706464+Krovcia@users.noreply.github.com>
2024-01-24 00:43:04 +0700

    Update Jadiun.lua

--- Jadiun.lua
+++ Jadiun.lua
@@ -1,3 +1,10 @@
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new( 2722, -115, -1991, 0, 0, -1, 0, 1, 0, 1, 0, 0)
+wait(1)
+local args = {[1] = "Script448Level",[2] = "-1",[3] = false}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Input"):FireServer(unpack(args))
+wait(1)
+local args = {[1] = "Script448Start"}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Input"):FireServer(unpack(args))
 wait(15)
 local args = {[1] = "SpeedChange",[2] = true}
 game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Input"):FireServer(unpack(args))
@@ -20,7 +27,15 @@ wait()
 local args = {[1] = "Summon",[2] = {["Rotation"] = 0,["cframe"] = CFrame.new(73.04423522949219, 179.73898315429688, 1288.3583984375, 1, 0, 0, 0, 1, 0, 0, 0, 1),["Unit"] = "SSJBKK Goku"}}
 game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Input"):FireServer(unpack(args))
 
-wait(60)
+wait(30)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new( 2722, -115, -1991, 0, 0, -1, 0, 1, 0, 1, 0, 0)
+wait(1)
+local args = {[1] = "Script448Level",[2] = "-1",[3] = false}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Input"):FireServer(unpack(args))
+wait(1)
+local args = {[1] = "Script448Start"}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Input"):FireServer(unpack(args))
+wait(30)
 
 local args = {[1] = "Upgrade",[2] = workspace:WaitForChild("Unit"):WaitForChild("SSJBKK Goku")}
 game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Server"):InvokeServer(unpack(args))
