62a62e751d0479ee41048cd2fdd0d7d9031b5ec8
Krovcia <143706464+Krovcia@users.noreply.github.com>
2024-01-23 19:45:01 +0700

    Update Jadiun.lua

--- Jadiun.lua
+++ Jadiun.lua
@@ -24,3 +24,29 @@ wait(60)
 
 local args = {[1] = "Upgrade",[2] = workspace:WaitForChild("Unit"):WaitForChild("SSJBKK Goku")}
 game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Server"):InvokeServer(unpack(args))
+
+wait(10)
+local args = {[1] = "Summon",[2] = {["Rotation"] = 0,["cframe"] = CFrame.new(67.626220703125, 179.73898315429688, 1288.6119384765625, 1, 0, 0, 0, 1, 0, 0, 0, 1),["Unit"] = "SSJBKK Goku"}}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Input"):FireServer(unpack(args))
+
+wait(10)
+local args = {[1] = "Summon",[2] = {["Rotation"] = 0,["cframe"] = CFrame.new(70.58243560791016, 179.73898315429688, 1291.3724365234375, 1, 0, 0, 0, 1, 0, 0, 0, 1),["Unit"] = "SSJBKK Goku"}}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Input"):FireServer(unpack(args))
+
+wait(10)
+local args = {[1] = "Summon",[2] = {["Rotation"] = 0,["cframe"] = CFrame.new(67.40760803222656, 179.73898315429688, 1294.5504150390625, 1, 0, 0, 0, 1, 0, 0, 0, 1),["Unit"] = "SSJBKK Goku"}}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Input"):FireServer(unpack(args))
+
+wait(10)
+local args = {[1] = "Summon",[2] = {["Rotation"] = 0,["cframe"] = CFrame.new(64.17701721191406, 179.69444274902344, 1316.114013671875, 1, 0, 0, 0, 1, 0, 0, 0, 1),["Unit"] = "SSJBKK Goku"}}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Input"):FireServer(unpack(args))
+
+wait(10)
+local args = {[1] = "Summon",[2] = {["Rotation"] = 0,["cframe"] = CFrame.new(70.23764038085938, 179.69444274902344, 1322.1314697265625, 1, 0, 0, 0, 1, 0, 0, 0, 1),["Unit"] = "SSJBKK Goku"}}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Input"):FireServer(unpack(args))
+
+wait(10)
+local args = {[1] = "Summon",[2] = {["Rotation"] = 0,["cframe"] = CFrame.new(65.2757568359375, 179.69444274902344, 1320.208984375, 1, 0, 0, 0, 1, 0, 0, 0, 1),["Unit"] = "SSJBKK Goku"}}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Input"):FireServer(unpack(args))
+
+
