65fd448f6375f0a5fb5f7df6f2587d77d0f6db3f
Krovcia <143706464+Krovcia@users.noreply.github.com>
2024-01-24 11:22:33 +0700

    Update Jadiun.lua

--- Jadiun.lua
+++ Jadiun.lua
@@ -20,7 +20,32 @@ wait()
 local args = {[1] = "Summon",[2] = {["Rotation"] = 0,["cframe"] = CFrame.new(73.04423522949219, 179.73898315429688, 1288.3583984375, 1, 0, 0, 0, 1, 0, 0, 0, 1),["Unit"] = "SSJBKK Goku"}}
 game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Input"):FireServer(unpack(args))
 
-wait(30)
+wait(15)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new( 2722, -115, -1991, 0, 0, -1, 0, 1, 0, 1, 0, 0)
+wait(1)
+local args = {[1] = "Script448Level",[2] = "-1",[3] = false}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Input"):FireServer(unpack(args))
+wait(1)
+local args = {[1] = "Script448Start"}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Input"):FireServer(unpack(args))
+wait(15)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new( 2722, -115, -1991, 0, 0, -1, 0, 1, 0, 1, 0, 0)
+wait(1)
+local args = {[1] = "Script448Level",[2] = "-1",[3] = false}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Input"):FireServer(unpack(args))
+wait(1)
+local args = {[1] = "Script448Start"}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Input"):FireServer(unpack(args))
+wait(15)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new( 2722, -115, -1991, 0, 0, -1, 0, 1, 0, 1, 0, 0)
+wait(1)
+local args = {[1] = "Script448Level",[2] = "-1",[3] = false}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Input"):FireServer(unpack(args))
+wait(1)
+local args = {[1] = "Script448Start"}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Input"):FireServer(unpack(args))
+wait(15)
+
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new( 2722, -115, -1991, 0, 0, -1, 0, 1, 0, 1, 0, 0)
 wait(1)
 local args = {[1] = "Script448Level",[2] = "-1",[3] = false}
