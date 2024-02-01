51a015647f0f06714b61f0c19014390d3e83693f
Krovcia <143706464+Krovcia@users.noreply.github.com>
2024-02-01 09:49:46 +0700

    Update ICE.lua

--- ICE.lua
+++ ICE.lua
@@ -4,5 +4,5 @@ VirtualInputManager:SendMouseButtonEvent(320, 160, 0, true, game, 1)
 VirtualInputManager:SendMouseMoveEvent(320, 500, game)
 wait(1)
 VirtualInputManager:SendMouseButtonEvent(320, 160, 0, false, game, 1)
-until false
+until(false)
 
