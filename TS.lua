007fdb3293542e99d0cc04601e03cc2a21ee8d92
Krovcia <143706464+Krovcia@users.noreply.github.com>
2024-02-01 10:17:20 +0700

    Update TS.lua

--- TS.lua
+++ TS.lua
@@ -1,4 +1,4 @@
-wait(90)
+repeat
 local args = {[1] = "TSRubber",[2] = "JetPistol",[3] = {}
 }
 game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
@@ -25,81 +25,5 @@ wait(2)
 local args = {[1] = "TSRubber",[2] = "RedHawk",[3] = {}}
 game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
 wait(2)
-local args = {[1] = "TSRubber",[2] = "JetPistol",[3] = {}
-}
-game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
-wait(2)
-local args = {[1] = "TSRubber",[2] = "JetGatling",[3] = {
-["MouseRay"] = {
-["Normal"] = Vector3.yAxis,
-["Direction"] = Vector3.new(262.9256896972656, -84.21864318847656, 117.3768081665039),
-["Origin"] = Vector3.new(52.63025665283203, 711.8206176757812, 1328.2266845703125),
-["Instance"] = workspace:WaitForChild("Map"):WaitForChild("Part"),
-["Distance"] = 100.19544219970703,
-["Material"] = Enum.Material.Ice,
-["Position"] = Vector3.new(140.44345092773438, 683.69287109375, 1367.4287109375) }}}
-game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
-wait(2)
-local args = {[1] = "TSRubber",[2] = "GrizzlyMagnum",[3] = {
-["MouseRay"] = {["Normal"] = Vector3.new(-0.027638092637062073, 0.9992260932922363, -0.02798713743686676),
-["Direction"] = Vector3.new(252.52734375, -144.88388061523438, 72.37831115722656),
-["Origin"] = Vector3.new(362.0897216796875, 764.4708251953125, -689.98046875),
-["Instance"] = workspace:WaitForChild("SafeZones"):WaitForChild("Dressrosa"):WaitForChild("MainColor"),["Distance"] = 60.790794372558594,
-["Material"] = Enum.Material.Slate,["Position"] = Vector3.new(413.2608337402344, 735.1121215820312, -675.3140258789062)}}}
-game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
-wait(2)
-local args = {[1] = "TSRubber",[2] = "RedHawk",[3] = {}}
-game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
-wait(2)
-local args = {[1] = "TSRubber",[2] = "JetPistol",[3] = {}
-}
-game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
-wait(2)
-local args = {[1] = "TSRubber",[2] = "JetGatling",[3] = {
-["MouseRay"] = {
-["Normal"] = Vector3.yAxis,
-["Direction"] = Vector3.new(262.9256896972656, -84.21864318847656, 117.3768081665039),
-["Origin"] = Vector3.new(52.63025665283203, 711.8206176757812, 1328.2266845703125),
-["Instance"] = workspace:WaitForChild("Map"):WaitForChild("Part"),
-["Distance"] = 100.19544219970703,
-["Material"] = Enum.Material.Ice,
-["Position"] = Vector3.new(140.44345092773438, 683.69287109375, 1367.4287109375) }}}
-game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
-wait(2)
-local args = {[1] = "TSRubber",[2] = "GrizzlyMagnum",[3] = {
-["MouseRay"] = {["Normal"] = Vector3.new(-0.027638092637062073, 0.9992260932922363, -0.02798713743686676),
-["Direction"] = Vector3.new(252.52734375, -144.88388061523438, 72.37831115722656),
-["Origin"] = Vector3.new(362.0897216796875, 764.4708251953125, -689.98046875),
-["Instance"] = workspace:WaitForChild("SafeZones"):WaitForChild("Dressrosa"):WaitForChild("MainColor"),["Distance"] = 60.790794372558594,
-["Material"] = Enum.Material.Slate,["Position"] = Vector3.new(413.2608337402344, 735.1121215820312, -675.3140258789062)}}}
-game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
-wait(2)
-local args = {[1] = "TSRubber",[2] = "RedHawk",[3] = {}}
-game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
-wait(2)
-local args = {[1] = "TSRubber",[2] = "JetPistol",[3] = {}
-}
-game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
-wait(2)
-local args = {[1] = "TSRubber",[2] = "JetGatling",[3] = {
-["MouseRay"] = {
-["Normal"] = Vector3.yAxis,
-["Direction"] = Vector3.new(262.9256896972656, -84.21864318847656, 117.3768081665039),
-["Origin"] = Vector3.new(52.63025665283203, 711.8206176757812, 1328.2266845703125),
-["Instance"] = workspace:WaitForChild("Map"):WaitForChild("Part"),
-["Distance"] = 100.19544219970703,
-["Material"] = Enum.Material.Ice,
-["Position"] = Vector3.new(140.44345092773438, 683.69287109375, 1367.4287109375) }}}
-game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
-wait(2)
-local args = {[1] = "TSRubber",[2] = "GrizzlyMagnum",[3] = {
-["MouseRay"] = {["Normal"] = Vector3.new(-0.027638092637062073, 0.9992260932922363, -0.02798713743686676),
-["Direction"] = Vector3.new(252.52734375, -144.88388061523438, 72.37831115722656),
-["Origin"] = Vector3.new(362.0897216796875, 764.4708251953125, -689.98046875),
-["Instance"] = workspace:WaitForChild("SafeZones"):WaitForChild("Dressrosa"):WaitForChild("MainColor"),["Distance"] = 60.790794372558594,
-["Material"] = Enum.Material.Slate,["Position"] = Vector3.new(413.2608337402344, 735.1121215820312, -675.3140258789062)}}}
-game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
-wait(2)
-local args = {[1] = "TSRubber",[2] = "RedHawk",[3] = {}}
-game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorNoYield"):FireServer(unpack(args))
-wait(2)
+keypress(0x57)
+until(false)
