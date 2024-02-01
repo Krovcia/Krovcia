f43718e9ab2e754994f18aadcf6e8eda2fe9524c
Krovcia <143706464+Krovcia@users.noreply.github.com>
2024-02-01 22:08:03 +0700

    Update kozvi.lua

--- kozvi.lua
+++ kozvi.lua
@@ -1,7 +1,7 @@
 wait(1)
 local args = {[1] = "Summon",[2] = {["Rotation"] = 0,["cframe"] = CFrame.new(7.6584625244140625, 899.4207153320312, -103.0057373046875, 1, 0, 0, 0, 1, 0, 0, 0, 1),["Unit"] = "Speedwagon"}}
 game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Input"):FireServer(unpack(args))
-wait(45)
+wait(50)
 local args = {
     [1] = "Upgrade",
     [2] = workspace:WaitForChild("Unit"):WaitForChild("Speedwagon")
