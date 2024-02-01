7fe6bdc4989652e2218231bdfb96611e672b1bfa
Krovcia <143706464+Krovcia@users.noreply.github.com>
2024-02-01 22:12:30 +0700

    Update kozvi.lua

--- kozvi.lua
+++ kozvi.lua
@@ -6,15 +6,7 @@ local args = {[1] = "Upgrade",[2] = workspace:WaitForChild("Unit"):WaitForChild(
 game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Server"):InvokeServer(unpack(args))
 local args = {[1] = "Upgrade",[2] = workspace:WaitForChild("Unit"):WaitForChild("Speedwagon")}
 game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Server"):InvokeServer(unpack(args))
-wait(32)
-local args = {
-    [1] = "Summon",
-    [2] = {
-        ["Rotation"] = 0,
-        ["cframe"] = CFrame.new(13.396768569946289, 899.5191650390625, -51.26992416381836, 1, 0, 0, 0, 1, 0, 0, 0, 1),
-        ["Unit"] = "Goku_Vegeta_SSJ_5Star"
-    }
-}
-
+wait(35)
+local args = {[1] = "Summon",[2] = {["Rotation"] = 0,["cframe"] = CFrame.new(13.396768569946289, 899.5191650390625, -51.26992416381836, 1, 0, 0, 0, 1, 0, 0, 0, 1),["Unit"] = "Goku_Vegeta_SSJ_5Star"}}
 game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Input"):FireServer(unpack(args))
 
