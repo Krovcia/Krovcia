508f85f3224508dc9a66740b21888d7a52cf745f
Krovcia <143706464+Krovcia@users.noreply.github.com>
2024-03-09 20:34:58 +0700

    Create ALSauto.lua

---
+++ ALSauto.lua
@@ -0,0 +1,118 @@
+wait(5)
+local args = {
+    [1] = "ONEHUNDREDMILLION"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ClaimCode"):InvokeServer(unpack(args))
+wait()
+local args = {
+    [1] = "BLAMSPOTBUGFIXCODEMUSTBESUBBEDTOWORK!"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ClaimCode"):InvokeServer(unpack(args))
+wait()
+local args = {
+    [1] = "Sub2MayyJeeeOrCodeWontWork"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ClaimCode"):InvokeServer(unpack(args))
+wait()
+local args = {
+    [1] = "Sub2Noclypso176k"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ClaimCode"):InvokeServer(unpack(args))
+wait()
+local args = {
+    [1] = "Sub2ZerozKinger1MilSubs"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ClaimCode"):InvokeServer(unpack(args))
+wait()
+local args = {
+    [1] = "Sub2KingLuffy"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ClaimCode"):InvokeServer(unpack(args))
+wait()
+local args = {
+    [1] = "Update3"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ClaimCode"):InvokeServer(unpack(args))
+wait()
+wait()
+local args = {
+    [1] = "Glitched"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ClaimCode"):InvokeServer(unpack(args))
+wait()
+local args = {
+    [1] = "Blams100kLikesCode"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ClaimCode"):InvokeServer(unpack(args))
+wait()
+local args = {
+    [1] = "BlamSpotsOP75MillVisitCodeMustBeSubbedToWork"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ClaimCode"):InvokeServer(unpack(args))
+
+local args = {
+    [1] = "10",
+    [2] = "1"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Summon"):InvokeServer(unpack(args))
+wait(1)
+local args = {
+    [1] = "10",
+    [2] = "1"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Summon"):InvokeServer(unpack(args))
+wait(1)
+local args = {
+    [1] = "10",
+    [2] = "1"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Summon"):InvokeServer(unpack(args))
+wait(1)
+local args = {
+    [1] = "10",
+    [2] = "1"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Summon"):InvokeServer(unpack(args))
+wait(1)
+local args = {
+    [1] = "10",
+    [2] = "1"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Summon"):InvokeServer(unpack(args))
+wait(1)
+local args = {
+    [1] = "10",
+    [2] = "1"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Summon"):InvokeServer(unpack(args))
+wait(1)
+local args = {
+    [1] = "10",
+    [2] = "1"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Summon"):InvokeServer(unpack(args))
+wait(1)
+local args = {
+    [1] = "10",
+    [2] = "1"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Summon"):InvokeServer(unpack(args))
+wait(1)
