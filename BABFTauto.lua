ed2e8d40c1f116560ee1ba631bd9aab863a6835b
Krovcia <143706464+Krovcia@users.noreply.github.com>
2024-04-12 22:35:09 +0700

    Update BABFTauto.lua

--- BABFTauto.lua
+++ BABFTauto.lua
@@ -16,41 +16,22 @@ local Tab = Window:CreateTab("Tab Example", 4483362458) -- Title, Image
 local Button = Tab:CreateButton({
    Name = "SORU",
    Callback = function()
-local args = {
-    [1] = "Painting Tool",
-    [2] = 1
-}
+local args = {[1] = "Painting Tool",[2] = 1}
 workspace:WaitForChild("ItemBoughtFromShop"):InvokeServer(unpack(args))
-wait()
-local args = {
-    [1] = "Binding Tool",
-    [2] = 1
-}
+wait(0.5)
+local args = {[1] = "Binding Tool",[2] = 1}
 workspace:WaitForChild("ItemBoughtFromShop"):InvokeServer(unpack(args))
-wait()
-local args = {
-    [1] = "Property Tool",
-    [2] = 1
-}
+wait(0.5)
+local args = {[1] = "Property Tool",[2] = 1}
 workspace:WaitForChild("ItemBoughtFromShop"):InvokeServer(unpack(args))
-wait()
-local args = {
-    [1] = "Scaling Tool",
-    [2] = 1
-}
+wait(0.5)
+local args = {[1] = "Scaling Tool",[2] = 1}
 workspace:WaitForChild("ItemBoughtFromShop"):InvokeServer(unpack(args))
-wait()
-local args = {
-    [1] = "Trowel Tool",
-    [2] = 1
-}
+wait(0.5)
+local args = {[1] = "Trowel Tool",[2] = 1}
 workspace:WaitForChild("ItemBoughtFromShop"):InvokeServer(unpack(args))
-wait()
-local args = {
-    [1] = "Legendary Chest",
-    [2] = 10
-}
-workspace:WaitForChild("ItemBoughtFromShop"):InvokeServer(unpack(args)
-
+wait(0.5)
+local args = {[1] = "Legendary Chest",[2] = 10}
+workspace:WaitForChild("ItemBoughtFromShop"):InvokeServer(unpack(args))
    end,
 })
