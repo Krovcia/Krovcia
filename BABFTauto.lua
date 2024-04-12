2eb891a14a160b224fbb63d76b2aa4d0b4d43a78
Krovcia <143706464+Krovcia@users.noreply.github.com>
2024-04-12 07:49:55 +0700

    Update BABFTauto.lua

--- BABFTauto.lua
+++ BABFTauto.lua
@@ -20,44 +20,40 @@ local args = {
     [1] = "Painting Tool",
     [2] = 1
 }
-
 workspace:WaitForChild("ItemBoughtFromShop"):InvokeServer(unpack(args))
 wait()
 local args = {
     [1] = "Binding Tool",
     [2] = 1
 }
-
 workspace:WaitForChild("ItemBoughtFromShop"):InvokeServer(unpack(args))
 wait()
 local args = {
     [1] = "Property Tool",
     [2] = 1
 }
-
 workspace:WaitForChild("ItemBoughtFromShop"):InvokeServer(unpack(args))
 wait()
 local args = {
     [1] = "Scaling Tool",
     [2] = 1
 }
-
 workspace:WaitForChild("ItemBoughtFromShop"):InvokeServer(unpack(args))
 wait()
 local args = {
     [1] = "Trowel Tool",
     [2] = 1
 }
-
 workspace:WaitForChild("ItemBoughtFromShop"):InvokeServer(unpack(args))
 wait()
 local args = {
     [1] = "Legendary Chest",
     [2] = 10
 }
-
 workspace:WaitForChild("ItemBoughtFromShop"):InvokeServer(unpack(args))
-
    end,
 })
+
+
+
 
