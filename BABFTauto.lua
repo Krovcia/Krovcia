7ec611b6ccde54969317283deb400950c62bd4b2
Krovcia <143706464+Krovcia@users.noreply.github.com>
2024-04-12 22:10:39 +0700

    Update BABFTauto.lua

--- BABFTauto.lua
+++ BABFTauto.lua
@@ -1,21 +1,4 @@
-repeat wait() until game:IsLoaded()
-wait(5)
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
-local Window = Rayfield:CreateWindow({
-   Name = "Rayfield Example Window",
-   LoadingTitle = "Rayfield Interface Suite",
-   LoadingSubtitle = "by Sirius",
-   ConfigurationSaving = {
-      Enabled = true,
-      FolderName = nil, -- Create a custom folder for your hub/game
-      FileName = "Big Hub"
-   }
-})
-local Tab = Window:CreateTab("Tab Example", 4483362458) -- Title, Image
 
-local Button = Tab:CreateButton({
-   Name = "1ST",
-   Callback = function()
 local args = {
     [1] = "Painting Tool",
     [2] = 1
@@ -51,11 +34,6 @@ local args = {
     [2] = 10
 }
 workspace:WaitForChild("ItemBoughtFromShop"):InvokeServer(unpack(args)
-
-   end,
-})
-
-)
 
 
 
