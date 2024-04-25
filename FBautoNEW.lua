a3c66d4c64309b1e43752a523de4d32e50b810e5
Krovcia <143706464+Krovcia@users.noreply.github.com>
2024-04-25 17:57:00 +0700

    Update FBautoNEW.lua

--- FBautoNEW.lua
+++ FBautoNEW.lua
@@ -19,6 +19,11 @@ VirtualInputManager:SendMouseButtonEvent(820, 130, 0, true, game, 1)
 VirtualInputManager:SendMouseButtonEvent(820, 130, 0, false, game, 1)
 -----------------
 wait(0.5)
+--Activate Skill--
+VirtualInputManager:SendMouseButtonEvent(500, 400, 0, true, game, 1)
+VirtualInputManager:SendMouseButtonEvent(500, 400, 0, false, game, 1)
+------------------
+wait(0.5)
 --INVENTORY GUI--
 VirtualInputManager:SendMouseButtonEvent(930, 10, 0, true, game, 1)
 VirtualInputManager:SendMouseButtonEvent(930, 10, 0, false, game, 1)
