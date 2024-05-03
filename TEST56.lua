da5c0e0b7c794912efe4d82e4dbf1f773ab5d013
Krovcia <143706464+Krovcia@users.noreply.github.com>
2024-05-03 08:26:26 +0700

    Update TEST56.lua

--- TEST56.lua
+++ TEST56.lua
@@ -1,35 +1,7 @@
 while wait() do
 local VirtualInputManager = game:GetService("VirtualInputManager")
 
-if game:GetService("Players").LocalPlayer.PlayerGui.UI.HUD.Visible == false then
-local VirtualInputManager = game:GetService("VirtualInputManager")
-wait(5)
-VirtualInputManager:SendMouseButtonEvent(500, 330, 0, true, game, 1)
-VirtualInputManager:SendMouseButtonEvent(500, 330, 0, false, game, 1)
-wait(3)
---INVENTORY GUI--
-VirtualInputManager:SendMouseButtonEvent(930, 10, 0, true, game, 1)
-VirtualInputManager:SendMouseButtonEvent(930, 10, 0, false, game, 1)
-wait(1)
-VirtualInputManager:SendMouseButtonEvent(820, 130, 0, true, game, 1)
-VirtualInputManager:SendMouseButtonEvent(820, 130, 0, false, game, 1)
------------------
-wait(0.5)
---Activate Skill--
-VirtualInputManager:SendMouseButtonEvent(500, 400, 0, true, game, 1)
-VirtualInputManager:SendMouseButtonEvent(500, 400, 0, false, game, 1)
-------------------
-wait(0.5)
---INVENTORY GUI--
-VirtualInputManager:SendMouseButtonEvent(930, 10, 0, true, game, 1)
-VirtualInputManager:SendMouseButtonEvent(930, 10, 0, false, game, 1)
-wait(1)
-VirtualInputManager:SendMouseButtonEvent(820, 90, 0, true, game, 1)
-VirtualInputManager:SendMouseButtonEvent(820, 90, 0, false, game, 1)
------------------
-wait(1)
-
-elseif game:GetService("Players").LocalPlayer.PlayerGui.UI.HUD.Visible == true then
+if game:GetService("Players").LocalPlayer.PlayerGui.UI.HUD.Visible == true then
 --Keypress 1--
 keypress(0x31)
 keyrelease(0x31)
@@ -90,6 +62,34 @@ wait(0.5)
 VirtualInputManager:SendMouseButtonEvent(500, 400, 0, true, game, 1)
 VirtualInputManager:SendMouseButtonEvent(500, 400, 0, false, game, 1)
 ------------------
+
+elseif game:GetService("Players").LocalPlayer.PlayerGui.UI.HUD.Visible == false then
+local VirtualInputManager = game:GetService("VirtualInputManager")
+wait(5)
+VirtualInputManager:SendMouseButtonEvent(500, 330, 0, true, game, 1)
+VirtualInputManager:SendMouseButtonEvent(500, 330, 0, false, game, 1)
+wait(3)
+--INVENTORY GUI--
+VirtualInputManager:SendMouseButtonEvent(930, 10, 0, true, game, 1)
+VirtualInputManager:SendMouseButtonEvent(930, 10, 0, false, game, 1)
+wait(1)
+VirtualInputManager:SendMouseButtonEvent(820, 130, 0, true, game, 1)
+VirtualInputManager:SendMouseButtonEvent(820, 130, 0, false, game, 1)
+-----------------
+wait(0.5)
+--Activate Skill--
+VirtualInputManager:SendMouseButtonEvent(500, 400, 0, true, game, 1)
+VirtualInputManager:SendMouseButtonEvent(500, 400, 0, false, game, 1)
+------------------
+wait(0.5)
+--INVENTORY GUI--
+VirtualInputManager:SendMouseButtonEvent(930, 10, 0, true, game, 1)
+VirtualInputManager:SendMouseButtonEvent(930, 10, 0, false, game, 1)
+wait(1)
+VirtualInputManager:SendMouseButtonEvent(820, 90, 0, true, game, 1)
+VirtualInputManager:SendMouseButtonEvent(820, 90, 0, false, game, 1)
+-----------------
+wait(1)
 
 if game:GetService("Players").LocalPlayer.Character.Stats:GetAttribute("Stamina") < 50 then
 keypress(0x1B)
