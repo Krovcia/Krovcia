c85c7ea80bf219504addfce97c7f39c93ab169a3
Krovcia <143706464+Krovcia@users.noreply.github.com>
2024-05-03 09:55:44 +0700

    Update TEST56.lua

--- TEST56.lua
+++ TEST56.lua
@@ -7,57 +7,57 @@ wait(0.5)
 keypress(0x31)
 keyrelease(0x31)
 --------------
-wait(0.5)
+wait(0.1)
 --Activate Skill--
 VirtualInputManager:SendMouseButtonEvent(500, 400, 0, true, game, 1)
 VirtualInputManager:SendMouseButtonEvent(500, 400, 0, false, game, 1)
 ------------------
-wait(0.5)
+wait(0.1)
 --Keypress 2--
 keypress(0x32)
 keyrelease(0x32)
 --------------
-wait(0.5)
+wait(0.1)
 --Activate Skill--
 VirtualInputManager:SendMouseButtonEvent(500, 400, 0, true, game, 1)
 VirtualInputManager:SendMouseButtonEvent(500, 400, 0, false, game, 1)
 ------------------
-wait(0.5)
+wait(0.1)
 --Keypress 3--
 keypress(0x33)
 keyrelease(0x33)
 --------------
-wait(0.5)
+wait(0.1)
 --Activate Skill--
 VirtualInputManager:SendMouseButtonEvent(500, 400, 0, true, game, 1)
 VirtualInputManager:SendMouseButtonEvent(500, 400, 0, false, game, 1)
 ------------------
-wait(0.5)
+wait(0.1)
 --4th Skill--
 VirtualInputManager:SendMouseButtonEvent(400, 350, 0, true, game, 1)
 VirtualInputManager:SendMouseButtonEvent(400, 350, 0, false, game, 1)
 -------------
-wait(0.5)
+wait(0.1)
 --Activate Skill--
 VirtualInputManager:SendMouseButtonEvent(500, 400, 0, true, game, 1)
 VirtualInputManager:SendMouseButtonEvent(500, 400, 0, false, game, 1)
 ------------------
-wait(0.5)
+wait(0.1)
 --5th Skill--
 VirtualInputManager:SendMouseButtonEvent(470, 350, 0, true, game, 1)
 VirtualInputManager:SendMouseButtonEvent(470, 350, 0, false, game, 1)
 -------------
-wait(0.5)
+wait(0.1)
 --Activate Skill--
 VirtualInputManager:SendMouseButtonEvent(500, 400, 0, true, game, 1)
 VirtualInputManager:SendMouseButtonEvent(500, 400, 0, false, game, 1)
 ------------------
-wait(0.5)
+wait(0.1)
 --6th Skill--
 VirtualInputManager:SendMouseButtonEvent(530, 350, 0, true, game, 1)
 VirtualInputManager:SendMouseButtonEvent(530, 350, 0, false, game, 1)
 -------------
-wait(0.5)
+wait(0.1)
 --Activate Skill--
 VirtualInputManager:SendMouseButtonEvent(500, 400, 0, true, game, 1)
 VirtualInputManager:SendMouseButtonEvent(500, 400, 0, false, game, 1)
