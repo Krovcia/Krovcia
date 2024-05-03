4281176a1aa7e7df0bb0eac4126f71a8c319931d
Krovcia <143706464+Krovcia@users.noreply.github.com>
2024-05-03 13:33:11 +0700

    Update TEST56.lua

--- TEST56.lua
+++ TEST56.lua
@@ -76,5 +76,13 @@ wait(1)
 VirtualInputManager:SendMouseButtonEvent(820, 130, 0, true, game, 1)
 VirtualInputManager:SendMouseButtonEvent(820, 130, 0, false, game, 1)
 -----------------
-end 
+end
+if game:GetService("Players").LocalPlayer.Character.Stats:GetAttribute("Stamina") < 50 then
+keypress(0x1B)
+wait(0.5)
+keypress(0x52)
+wait(0.5)
+keypress(0x0D)
+keyrelease(0x0D)
+  end
 end
