f91017e405295ff7ca717f4390e6b1fc68413ee7
Krovcia <143706464+Krovcia@users.noreply.github.com>
2024-05-03 18:06:47 +0700

    Update TEST57.lua

--- TEST57.lua
+++ TEST57.lua
@@ -1,88 +1,8 @@
-while wait(1) do
-local VirtualInputManager = game:GetService("VirtualInputManager")
-if game:GetService("Players").LocalPlayer.Character.Stats:GetAttribute("Stamina") > 50 then
-wait(0.5)
---Keypress 1--
-keypress(0x31)
-keyrelease(0x31)
---------------
-wait(0.1)
---Activate Skill--
-VirtualInputManager:SendMouseButtonEvent(500, 400, 0, true, game, 1)
-VirtualInputManager:SendMouseButtonEvent(500, 400, 0, false, game, 1)
-------------------
-wait(0.1)
---Keypress 2--
-keypress(0x32)
-keyrelease(0x32)
---------------
-wait(0.1)
---Activate Skill--
-VirtualInputManager:SendMouseButtonEvent(500, 400, 0, true, game, 1)
-VirtualInputManager:SendMouseButtonEvent(500, 400, 0, false, game, 1)
-------------------
-wait(0.1)
---Keypress 3--
-keypress(0x33)
-keyrelease(0x33)
---------------
-wait(0.1)
---Activate Skill--
-VirtualInputManager:SendMouseButtonEvent(500, 400, 0, true, game, 1)
-VirtualInputManager:SendMouseButtonEvent(500, 400, 0, false, game, 1)
-------------------
-wait(0.1)
---4th Skill--
-VirtualInputManager:SendMouseButtonEvent(400, 350, 0, true, game, 1)
-VirtualInputManager:SendMouseButtonEvent(400, 350, 0, false, game, 1)
--------------
-wait(0.1)
---Activate Skill--
-VirtualInputManager:SendMouseButtonEvent(500, 400, 0, true, game, 1)
-VirtualInputManager:SendMouseButtonEvent(500, 400, 0, false, game, 1)
-------------------
-wait(0.1)
---5th Skill--
-VirtualInputManager:SendMouseButtonEvent(470, 350, 0, true, game, 1)
-VirtualInputManager:SendMouseButtonEvent(470, 350, 0, false, game, 1)
--------------
-wait(0.1)
---Activate Skill--
-VirtualInputManager:SendMouseButtonEvent(500, 400, 0, true, game, 1)
-VirtualInputManager:SendMouseButtonEvent(500, 400, 0, false, game, 1)
-------------------
-wait(0.1)
---6th Skill--
-VirtualInputManager:SendMouseButtonEvent(530, 350, 0, true, game, 1)
-VirtualInputManager:SendMouseButtonEvent(530, 350, 0, false, game, 1)
--------------
-wait(0.1)
---Activate Skill--
-VirtualInputManager:SendMouseButtonEvent(500, 400, 0, true, game, 1)
-VirtualInputManager:SendMouseButtonEvent(500, 400, 0, false, game, 1)
-------------------
-elseif game:GetService("Players").LocalPlayer.Character.Stats:GetAttribute("Stamina") < 50 then
+if game:GetService("Players").LocalPlayer.Character.Stats:GetAttribute("Stamina") < 50 then
 keypress(0x1B)
 wait(0.5)
 keypress(0x52)
 wait(0.5)
 keypress(0x0D)
 keyrelease(0x0D)
-
-else
-game.StarterGui:SetCoreGuiEnabled(2, true)
-wait(1)
-VirtualInputManager:SendMouseButtonEvent(500, 330, 0, true, game, 1)
-VirtualInputManager:SendMouseButtonEvent(500, 330, 0, false, game, 1)
-wait(3)
-game.StarterGui:SetCoreGuiEnabled(2, true)
-wait(1)
-VirtualInputManager:SendMouseButtonEvent(930, 10, 0, true, game, 1)
-VirtualInputManager:SendMouseButtonEvent(930, 10, 0, false, game, 1)
-wait(1)
-VirtualInputManager:SendMouseButtonEvent(820, 130, 0, true, game, 1)
-VirtualInputManager:SendMouseButtonEvent(820, 130, 0, false, game, 1)
------------------
 end
-end
-
