3c878104b6a28e99233634df4f4a2c6b2d72b3b7
Krovcia <143706464+Krovcia@users.noreply.github.com>
2024-05-04 00:17:55 +0700

    Update TEST57.lua

--- TEST57.lua
+++ TEST57.lua
@@ -1,4 +1,4 @@
-while wait(1) do
+while wait(game:GetService("Players").LocalPlayer.PlayerGui.UI.HUD.Visible = true) do
 if game:GetService("Players").LocalPlayer.Character.Stats:GetAttribute("Stamina") < 200 then
 keypress(0x1B)
 wait(0.5)
