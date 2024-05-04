27a3a728ccd2e43d71515c65a157acb096e3285a
Krovcia <143706464+Krovcia@users.noreply.github.com>
2024-05-04 09:47:33 +0700

    Update TEST57.lua

--- TEST57.lua
+++ TEST57.lua
@@ -1,3 +1,20 @@
+
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local Window = Rayfield:CreateWindow({
+   Name = "Rayfield Example Window",
+   LoadingTitle = "Rayfield Interface Suite",
+   LoadingSubtitle = "by Sirius",
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = nil, -- Create a custom folder for your hub/game
+      FileName = "Big Hub"
+   }
+})
+local Tab = Window:CreateTab("Tab Example", 4483362458) -- Title, Image
+
+local Button = Tab:CreateButton({
+   Name = "SORU",
+   Callback = function()
 if game:GetService("Players").LocalPlayer.Character.Stats:GetAttribute("Stamina") < 200 then
 keypress(0x1B)
 wait(0.5)
@@ -6,4 +23,7 @@ wait(0.5)
 keypress(0x0D)
 keyrelease(0x0D)
 end
+   end,
+})
+
 
