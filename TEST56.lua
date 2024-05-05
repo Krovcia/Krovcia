35f01257fd26d9e0270592f5b2ead6b91126127b
Krovcia <143706464+Krovcia@users.noreply.github.com>
2024-05-06 03:16:48 +0700

    Update TEST56.lua

--- TEST56.lua
+++ TEST56.lua
@@ -74,12 +74,24 @@ VirtualInputManager:SendMouseButtonEvent(500, 330, 0, true, game, 1)
 VirtualInputManager:SendMouseButtonEvent(500, 330, 0, false, game, 1)
 wait(4)
 game.StarterGui:SetCoreGuiEnabled(2, true)
-wait(1)
+wait(0.5)
+VirtualInputManager:SendMouseButtonEvent(930, 10, 0, true, game, 1)
+VirtualInputManager:SendMouseButtonEvent(930, 10, 0, false, game, 1)
+wait(0.5)
+VirtualInputManager:SendMouseButtonEvent(820, 130, 0, true, game, 1)
+VirtualInputManager:SendMouseButtonEvent(820, 130, 0, false, game, 1)
+wait(0.5)
+--Activate Skill--
+VirtualInputManager:SendMouseButtonEvent(500, 400, 0, true, game, 1)
+VirtualInputManager:SendMouseButtonEvent(500, 400, 0, false, game, 1)
+------------------
+wait(0.5)
+--INVENTORY GUI--
 VirtualInputManager:SendMouseButtonEvent(930, 10, 0, true, game, 1)
 VirtualInputManager:SendMouseButtonEvent(930, 10, 0, false, game, 1)
 wait(1)
-VirtualInputManager:SendMouseButtonEvent(820, 130, 0, true, game, 1)
-VirtualInputManager:SendMouseButtonEvent(820, 130, 0, false, game, 1)
+VirtualInputManager:SendMouseButtonEvent(820, 90, 0, true, game, 1)
+VirtualInputManager:SendMouseButtonEvent(820, 90, 0, false, game, 1)
 -----------------
 end
 end
