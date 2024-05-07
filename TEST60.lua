52d8905099c6bf0cbb8859b3ac423f31788363d8
Krovcia <143706464+Krovcia@users.noreply.github.com>
2024-05-07 12:58:03 +0700

    Create TEST60.lua

---
+++ TEST60.lua
@@ -0,0 +1,5 @@
+while wait(300) do
+if game:GetService("Players").LocalPlayer.PlayerGui.UI.HUD.Visible == true then 
+                   game:GetService("Players").LocalPlayer.Character.Humanoid.Health = 0
+  end
+end
