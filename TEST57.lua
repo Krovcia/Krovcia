2877f808cff8ebd784033a1ffc80b2d2f4518f5d
Krovcia <143706464+Krovcia@users.noreply.github.com>
2024-05-08 09:45:27 +0700

    Update TEST57.lua

--- TEST57.lua
+++ TEST57.lua
@@ -1,21 +1,6 @@
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "Title of the library", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+wait()
+if game:GetService("Players").LocalPlayer.Character.Stats:GetAttribute("Stamina") < 50 then
+end
 
-local Tab = Window:MakeTab({
-	Name = "Tab 1",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
+                   
 
-local Section = Tab:AddSection({
-	Name = "Section"
-})
-
-Tab:AddButton({
-	Name = "Button!",
-	Callback = function()
-      		if game:GetService("Players").LocalPlayer.Character.Stats:GetAttribute("Stamina") < 50 then
-                   game:GetService("Players").LocalPlayer.Character.Humanoid.Health = 0
-		end
-  	end    
-})
