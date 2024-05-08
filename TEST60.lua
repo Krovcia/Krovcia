ee44203c41e5fa9e0879d8eade296ffcf7c19595
Krovcia <143706464+Krovcia@users.noreply.github.com>
2024-05-09 04:31:39 +0700

    Update TEST60.lua

--- TEST60.lua
+++ TEST60.lua
@@ -1,3 +1,5 @@
+--5min reset--
+
 while wait(300) do
 if game:GetService("Players").LocalPlayer.PlayerGui.UI.HUD.Visible == true then 
                    game:GetService("Players").LocalPlayer.Character.Humanoid.Health = 0
