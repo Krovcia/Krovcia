dba669fbff37cb04dafa2ac419ef48ec52ab3a68
Krovcia <143706464+Krovcia@users.noreply.github.com>
2024-05-09 10:18:15 +0700

    Update TEST57.lua

--- TEST57.lua
+++ TEST57.lua
@@ -2,7 +2,7 @@ wait(5)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Krovcia/Krovcia/main/TEST59.lua"))();
 local VirtualInputManager = game:GetService("VirtualInputManager");
 
-while wait(0.1) do
+while wait(10) do
       local z = string.sub((game:GetService("Players").LocalPlayer.PlayerGui.UI.Info.ServerTime.Text), 15, 20)
       local y = string.gsub(z, ":", "")
       local y = tonumber(y)
