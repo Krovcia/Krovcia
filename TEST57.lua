3f51b4596871a790e6aa0286c782d1f55f61a4f3
Krovcia <143706464+Krovcia@users.noreply.github.com>
2024-05-09 08:15:49 +0700

    Update TEST57.lua

--- TEST57.lua
+++ TEST57.lua
@@ -114,6 +114,10 @@ game.StarterGui:SetCoreGuiEnabled(2, true)
     keypress(0x4A)
     end
 
+    local z = string.sub((game:GetService("Players").LocalPlayer.PlayerGui.UI.Info.ServerTime.Text), 15, 20)
+    local y = string.gsub(z, ":", "")
+    local y = tonumber(y)
+
     if y > 1000 and y < 5000 then
         keypress(0x4B)
     elseif y > 10 and y < 1000 then
