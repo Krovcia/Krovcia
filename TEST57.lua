6d69f24c1daeffae40150f033ec634c100d80fd3
Krovcia <143706464+Krovcia@users.noreply.github.com>
2024-05-09 10:11:35 +0700

    Update TEST57.lua

--- TEST57.lua
+++ TEST57.lua
@@ -1,3 +1,6 @@
 wait(5)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Krovcia/Krovcia/main/TEST59.lua"))();
 
+while wait(5) do
+VirtualInputManager:SendMouseButtonEvent(500, 230, 0, true, game, 1)
+VirtualInputManager:SendMouseButtonEvent(500, 230, 0, false, game, 1)
