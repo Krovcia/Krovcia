e8fcb4056c124c166b565e74cf07cd638a523a9f
Krovcia <143706464+Krovcia@users.noreply.github.com>
2024-05-09 11:11:45 +0700

    Update TEST57.lua

--- TEST57.lua
+++ TEST57.lua
@@ -5,14 +5,11 @@ local VirtualInputManager = game:GetService("VirtualInputManager");
 while wait(10) do
 
 if game:GetService("Players").LocalPlayer.Character == nil then
-    elseif game:GetService("Players").LocalPlayer.PlayerGui.UI.HUD.Visible == true then
+    elseif 
       local z = string.sub((game:GetService("Players").LocalPlayer.PlayerGui.UI.Info.ServerTime.Text), 15, 20)
       local y = string.gsub(z, ":", "")
       local y = tonumber(y)
-      end
-
-if game:GetService("Players").LocalPlayer.Character == nil then
-    elseif game:GetService("Players").LocalPlayer.PlayerGui.UI.HUD.Visible == true and y > 500 and y < 5000 then
+game:GetService("Players").LocalPlayer.PlayerGui.UI.HUD.Visible == true and y > 500 and y < 5000 then
 keypress(0x4B)
 elseif y > 10 and y < 500 then
 VirtualInputManager:SendMouseButtonEvent(500, 230, 0, true, game, 1)
