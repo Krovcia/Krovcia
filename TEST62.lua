163f72da217f2b3f44460f065724ce33afd48fc5
Krovcia <143706464+Krovcia@users.noreply.github.com>
2024-05-09 19:20:41 +0700

    Update TEST62.lua

--- TEST62.lua
+++ TEST62.lua
@@ -1,22 +1,44 @@
 wait(60)
+
+local parent = game:GetService("Players").LocalPlayer.MAIN_DATA.Titles
+local children = parent:GetChildren() 
+local num = #children
+
 local gui = Instance.new("ScreenGui")
 gui.Parent = game.Players.LocalPlayer.PlayerGui -- This places the GUI in the player's screen
 
-local Boss = Instance.new("TextButton")
-Boss.Parent = gui
-Boss.Position = UDim2.new(0, 400, 0, 30)
-Boss.Size = UDim2.new(0, 40, 0, 40)
-Boss.Text = "Boss:"
 
 local BossKill = Instance.new("TextButton")
 BossKill.Parent = gui
-BossKill.Position = UDim2.new(0, 440, 0, 30)
-BossKill.Size = UDim2.new(0, 60, 0, 60)
+BossKill.Position = UDim2.new(0, 400, 0, 30)
+BossKill.Size = UDim2.new(0, 75, 0, 60)
 BossKill.TextSize = 25
+
+local Tittles = Instance.new("TextButton")
+Tittles.Parent = gui
+Tittles.Position = UDim2.new(0, 475, 0, 30)
+Tittles.Size = UDim2.new(0, 75, 0, 60)
+Tittles.TextSize = 25
+Tittles.Text = num
+
+local Tittle = Instance.new("TextButton")
+Tittle.Parent = gui
+Tittle.Position = UDim2.new(0, 475, 0, 20)
+Tittle.Size = UDim2.new(0, 75, 0, 20)
+Tittle.TextSize = 12
+Tittle.Text = "Tittle"
+
+local Boss = Instance.new("TextButton")
+Boss.Parent = gui
+Boss.Position = UDim2.new(0, 400, 0, 20)
+Boss.Size = UDim2.new(0, 75, 0, 20)
+Boss.TextSize = 12
+Boss.Text = "BOSS"
 
 local Servertime = Instance.new("TextButton")
 Servertime.Parent = gui
-Servertime.Position = UDim2.new(0, 400, 0, 10)
+Servertime.Position = UDim2.new(0, 400, 0, 0)
+Servertime.TextSize = 10
 Servertime.Size = UDim2.new(0, 150, 0, 20)
 
 while wait(0.1) do
