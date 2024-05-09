2b108ed224199f37baa1bb801194dd4487a2c2d3
Krovcia <143706464+Krovcia@users.noreply.github.com>
2024-05-09 10:22:35 +0700

    Update TEST57.lua

--- TEST57.lua
+++ TEST57.lua
@@ -6,9 +6,9 @@ while wait(10) do
       local z = string.sub((game:GetService("Players").LocalPlayer.PlayerGui.UI.Info.ServerTime.Text), 15, 20)
       local y = string.gsub(z, ":", "")
       local y = tonumber(y)
-    if y > 1000 and y < 5000 then
+    if y > 500 and y < 5000 then
 keypress(0x4B)
-    elseif y > 10 and y < 1000 then
+    elseif y > 10 and y < 500 then
 VirtualInputManager:SendMouseButtonEvent(500, 230, 0, true, game, 1)
 VirtualInputManager:SendMouseButtonEvent(500, 230, 0, false, game, 1)
       end
