701bbf859bc4802ea13012b0ee9b436a395b22e2
Krovcia <143706464+Krovcia@users.noreply.github.com>
2024-05-13 15:08:27 +0700

    Create ForFBGUI.lua

---
+++ ForFBGUI.lua
@@ -0,0 +1,55 @@
+local gui = Instance.new("ScreenGui")
+gui.Parent = game.Players.LocalPlayer.PlayerGui -- This places the GUI in the player's screen
+
+
+local NAMA = Instance.new("TextButton")
+NAMA.Parent = gui
+NAMA.Position = UDim2.new(0, 10, 0, 0)
+NAMA.Size = UDim2.new(0, 120, 0, 60)
+NAMA.TextSize = 25
+NAMA.Text = game:GetService("Players").LocalPlayer.DisplayName
+
+
+local BUAH = Instance.new("TextButton")
+BUAH.Parent = gui
+BUAH.Position = UDim2.new(0, 10, 0, 50)
+BUAH.Size = UDim2.new(0, 120, 0, 60)
+BUAH.TextSize = 25
+
+local GEMS = Instance.new("TextButton")
+GEMS.Parent = gui
+GEMS.Position = UDim2.new(0, 130, 0, 0)
+GEMS.Size = UDim2.new(0, 120, 0, 60)
+GEMS.TextSize = 25
+
+local DAPET = Instance.new("TextButton")
+DAPET.Parent = gui
+DAPET.Position = UDim2.new(0, 130, 0, 50)
+DAPET.Size = UDim2.new(0, 120, 0, 60)
+DAPET.TextSize = 25
+DAPET.MouseButton1Click:Connect(function()
+local args = {[1] = "FruitsHandler",[2] = "SwitchSlot",[3] = {["Slot"] = 2}}
+game:GetService("ReplicatedStorage"):WaitForChild("Replicator"):InvokeServer(unpack(args))
+end)
+
+
+while wait(1) do
+
+local buahbuah = game:GetService("Players").LocalPlayer.MAIN_DATA.Slots["1"].Value
+
+if buahbuah == "Dough" or buahbuah == "Lightning" or buahbuah == "Leopard" or buahbuah == "Ope" or buahbuah == "Venom" or buahbuah == "Nika" or buahbuah == "DragonV2" or buahbuah == "IceV2" or buahbuah == "MagmaV2" or buahbuah == "TSRubber" or buahbuah == "Soul" or buahbuah == "Dragon" or buahbuah == "DarkXQuake" then
+
+DAPET.BackgroundColor3 = Color3.fromRGB(0, 200, 0)
+    DAPET.Text = "YES"
+    GEMS.Text = game:GetService("Players").LocalPlayer.MAIN_DATA.Gems.Value
+    BUAH.Text = game:GetService("Players").LocalPlayer.MAIN_DATA.Slots["1"].Value
+
+
+else
+DAPET.BackgroundColor3 = Color3.fromRGB(200, 0, 0)
+DAPET.Text = "NO"
+    GEMS.Text = game:GetService("Players").LocalPlayer.MAIN_DATA.Gems.Value
+    BUAH.Text = game:GetService("Players").LocalPlayer.MAIN_DATA.Slots["1"].Value
+
+end
+end
