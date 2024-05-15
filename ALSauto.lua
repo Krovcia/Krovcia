6c25ba04c3e6b0149711c9f8c3b29a1480af59ab
Krovcia <143706464+Krovcia@users.noreply.github.com>
2024-05-15 15:27:00 +0700

    Update ALSauto.lua

--- ALSauto.lua
+++ ALSauto.lua
@@ -156,3 +156,41 @@ local args = {
 }game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ClaimCode"):InvokeServer(unpack(args))
 wait()
 
+repeat wait() until game:IsLoaded()
+wait(5)
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local Window = Rayfield:CreateWindow({
+   Name = "Rayfield Example Window",
+   LoadingTitle = "Rayfield Interface Suite",
+   LoadingSubtitle = "by Sirius",
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = nil, -- Create a custom folder for your hub/game
+      FileName = "Big Hub"
+   }
+})
+local Tab = Window:CreateTab("Tab Example", 4483362458) -- Title, Image
+
+local Button = Tab:CreateButton({
+   Name = "banner 2",
+   Callback = function()
+local args = {
+    [1] = "10",
+    [2] = "2"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Summon"):InvokeServer(unpack(args))
+   end,
+})
+
+local Button = Tab:CreateButton({
+   Name = "banner 1",
+   Callback = function()
+local args = {
+    [1] = "10",
+    [2] = "1"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Summon"):InvokeServer(unpack(args))
+   end,
+})
