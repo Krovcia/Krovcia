07bbb05b2c21a3233812a3cab80a23d6bd9a9184
Krovcia <143706464+Krovcia@users.noreply.github.com>
2024-05-15 20:51:22 +0700

    Create TEST57_1.lua

---
+++ TEST57_1.lua
@@ -0,0 +1,91 @@
+--AUTO LEVELING MACRO--
+
+wait(10)
+
+local VirtualInputManager = game:GetService("VirtualInputManager");
+while wait(0.1) do
+
+    if game:GetService("Players").LocalPlayer.PlayerGui.UI.HUD.Visible == true then
+        wait(0.1)
+game.StarterGui:SetCoreGuiEnabled(2, true)  
+        --Keypress 1--
+        keypress(0x31)
+        keyrelease(0x31)
+        --------------
+        wait(0.1)
+        --Activate Skill--
+        VirtualInputManager:SendMouseButtonEvent(500, 400, 0, true, game, 1)
+        VirtualInputManager:SendMouseButtonEvent(500, 400, 0, false, game, 1)
+        ------------------
+        wait(0.1)
+        --Keypress 2--
+        keypress(0x32)
+        keyrelease(0x32)
+        --------------
+        wait(0.1)
+        --Activate Skill--
+        VirtualInputManager:SendMouseButtonEvent(500, 400, 0, true, game, 1)
+        VirtualInputManager:SendMouseButtonEvent(500, 400, 0, false, game, 1)
+        ------------------
+        wait(0.1)
+        --Keypress 3--
+        keypress(0x33)
+        keyrelease(0x33)
+        --------------
+        wait(0.1)
+        --Activate Skill--
+        VirtualInputManager:SendMouseButtonEvent(500, 400, 0, true, game, 1)
+        VirtualInputManager:SendMouseButtonEvent(500, 400, 0, false, game, 1)
+        ------------------
+        wait(0.1)
+        --4th Skill--
+        VirtualInputManager:SendMouseButtonEvent(400, 350, 0, true, game, 1)
+        VirtualInputManager:SendMouseButtonEvent(400, 350, 0, false, game, 1)
+        -------------
+        wait(0.1)
+        --Activate Skill--
+        VirtualInputManager:SendMouseButtonEvent(500, 400, 0, true, game, 1)
+        VirtualInputManager:SendMouseButtonEvent(500, 400, 0, false, game, 1)
+        ------------------
+        wait(0.1)
+        --5th Skill--
+        VirtualInputManager:SendMouseButtonEvent(470, 350, 0, true, game, 1)
+        VirtualInputManager:SendMouseButtonEvent(470, 350, 0, false, game, 1)
+        -------------
+        wait(0.1)
+        --Activate Skill--
+        VirtualInputManager:SendMouseButtonEvent(500, 400, 0, true, game, 1)
+        VirtualInputManager:SendMouseButtonEvent(500, 400, 0, false, game, 1)
+        ------------------
+        wait(0.1)
+        --6th Skill--
+        VirtualInputManager:SendMouseButtonEvent(530, 350, 0, true, game, 1)
+        VirtualInputManager:SendMouseButtonEvent(530, 350, 0, false, game, 1)
+        -------------
+        wait(0.1)
+        --Activate Skill--
+        VirtualInputManager:SendMouseButtonEvent(500, 400, 0, true, game, 1)
+        VirtualInputManager:SendMouseButtonEvent(500, 400, 0, false, game, 1)
+    end
+
+    if game:GetService("Players").LocalPlayer.Character == nil then
+    elseif game:GetService("Players").LocalPlayer.PlayerGui.UI.HUD.Visible == true and game:GetService("Players").LocalPlayer.Character.Stats:GetAttribute("Stamina") < 50 then
+        game:GetService("Players").LocalPlayer.Character.Humanoid.Health = 0
+    end
+
+    if game:GetService("Players").LocalPlayer.PlayerGui.UI.HUD.Visible == true and game:GetService("CoreGui").RobloxGui.Backpack.Inventory.Visible == false then
+        game:GetService("CoreGui").RobloxGui.Backpack.Inventory.Visible = true     
+    end
+    
+    if game:GetService("Players").LocalPlayer.PlayerGui.UI.HUD.Visible == false then
+    wait(1)
+    VirtualInputManager:SendMouseButtonEvent(500, 330, 0, true, game, 1)
+    VirtualInputManager:SendMouseButtonEvent(500, 330, 0, false, game, 1)
+    end
+
+    if game:GetService("Players").LocalPlayer.PlayerGui.UI.HUD.Visible == false and game:GetService("CoreGui").RobloxGui.Backpack.Inventory.Visible == true then
+    game:GetService("CoreGui").RobloxGui.Backpack.Inventory.Visible = false
+    end
+
+
+end
