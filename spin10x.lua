40c0d14df3aa3cb451c85f338511894007195d0b
Krovcia <143706464+Krovcia@users.noreply.github.com>
2024-05-18 13:38:33 +0700

    Update spin10x.lua

--- spin10x.lua
+++ spin10x.lua
@@ -35,3 +35,48 @@ local args = {
 game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Summon"):InvokeServer(unpack(args))
    end,
 })
+
+local Button = Tab:CreateButton({
+   Name = "Jenos Core",
+   Callback = function()
+local args = {[1] = "MythicSpiritShard",[2] = "LegendarySpiritShard",[3] = 1}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ConvertShards"):FireServer(unpack(args))
+
+local args = {[1] = "MythicSpiritShard",[2] = "LegendarySpiritShard",[3] = 1}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ConvertShards"):FireServer(unpack(args))
+
+
+local args = {[1] = "LegendarySpiritShard",[2] = "EpicSpiritShard",[3] = 1}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ConvertShards"):FireServer(unpack(args))
+
+local args = {[1] = "LegendarySpiritShard",[2] = "EpicSpiritShard",[3] = 1}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ConvertShards"):FireServer(unpack(args))
+
+local args = {[1] = "LegendarySpiritShard",[2] = "EpicSpiritShard",[3] = 1}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ConvertShards"):FireServer(unpack(args))
+
+local args = {[1] = "EpicSpiritShard",[2] = "RareSpiritShard",[3] = 1}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ConvertShards"):FireServer(unpack(args))
+
+local args = {[1] = "EpicSpiritShard",[2] = "RareSpiritShard",[3] = 1}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ConvertShards"):FireServer(unpack(args))
+
+local args = {[1] = "EpicSpiritShard",[2] = "RareSpiritShard",[3] = 1}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ConvertShards"):FireServer(unpack(args))
+
+local args = {[1] = "EpicSpiritShard",[2] = "RareSpiritShard",[3] = 1}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ConvertShards"):FireServer(unpack(args))
+
+local args = {[1] = "EpicSpiritShard",[2] = "RareSpiritShard",[3] = 1}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ConvertShards"):FireServer(unpack(args))
+
+wait(1)
+
+local args = {
+    [1] = 11
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("AttemptCraft"):FireServer(unpack(args))
+
+   end,
+})
