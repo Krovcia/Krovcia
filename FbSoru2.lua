47f0c61e3e72ca4da6035997bef1a496f889e108
Krovcia <143706464+Krovcia@users.noreply.github.com>
2024-05-19 23:14:12 +0700

    Update FbSoru2.lua

--- FbSoru2.lua
+++ FbSoru2.lua
@@ -1,9 +1,2 @@
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Krovcia/Krovcia/main/FbSOru.lua'))();
-wait(30)
-while wait(1) do
-    if game:GetService("Players").LocalPlayer.PlayerGui.UI.HUD.Visible == false then
-    wait(1)
-    VirtualInputManager:SendMouseButtonEvent(500, 330, 0, true, game, 1)
-    VirtualInputManager:SendMouseButtonEvent(500, 330, 0, false, game, 1)
-    end
-end
+
