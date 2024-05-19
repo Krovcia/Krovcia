1c342ba6a2b4e7f1308ac6e8bb9ed47f0fc8fe09
Krovcia <143706464+Krovcia@users.noreply.github.com>
2024-05-19 22:13:32 +0700

    Update FbSoru2.lua

--- FbSoru2.lua
+++ FbSoru2.lua
@@ -5,13 +5,6 @@ wait(10)
 local gui = Instance.new("ScreenGui")
 gui.Parent = game.Players.LocalPlayer.PlayerGui -- This places the GUI in the player's screen
 
-
-local BossKill = Instance.new("TextButton")
-BossKill.Parent = gui
-BossKill.Position = UDim2.new(0, 400, 0, 30)
-BossKill.Size = UDim2.new(0, 75, 0, 60)
-BossKill.TextSize = 25
-
 local Tittles = Instance.new("TextButton")
 Tittles.Parent = gui
 Tittles.Position = UDim2.new(0, 475, 0, 30)
@@ -26,17 +19,13 @@ Tittle.Size = UDim2.new(0, 75, 0, 20)
 Tittle.TextSize = 12
 Tittle.Text = "Tittle"
 
-local Boss = Instance.new("TextButton")
-Boss.Parent = gui
-Boss.Position = UDim2.new(0, 400, 0, 20)
-Boss.Size = UDim2.new(0, 75, 0, 20)
-Boss.TextSize = 12
-Boss.Text = "BOSS"
-
-
 while wait(0.1) do
 
-
+    if game:GetService("Players").LocalPlayer.PlayerGui.UI.HUD.Visible == false then
+    wait(1)
+    VirtualInputManager:SendMouseButtonEvent(500, 330, 0, true, game, 1)
+    VirtualInputManager:SendMouseButtonEvent(500, 330, 0, false, game, 1)
+    end
 
 if game:GetService("Players").LocalPlayer.Character == nil then
     elseif game:GetService("Players").LocalPlayer.PlayerGui.UI.HUD.Visible == true then
