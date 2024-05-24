6debf23a1aca0e3ff8e37cd99fe95487fb7c755b
Krovcia <143706464+Krovcia@users.noreply.github.com>
2024-05-24 11:13:49 +0700

    Update AUTOJOINALSraid2.lua

--- AUTOJOINALSraid2.lua
+++ AUTOJOINALSraid2.lua
@@ -1,17 +1,17 @@
 wait(0.1)
-local args = {[1] = "Ready",[2] = "Marines Fort",[3] = 5}
+local args = {[1] = "Ready",[2] = "Hell City",[3] = 5}
 game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Teleporter"):WaitForChild("MapSelect"):InvokeServer(unpack(args))
 wait(0.1)
-local args = {[1] = "Ready",[2] = "Marines Fort",[3] = 4}
+local args = {[1] = "Ready",[2] = "Hell City",[3] = 4}
 game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Teleporter"):WaitForChild("MapSelect"):InvokeServer(unpack(args))
 wait(0.1)
-local args = {[1] = "Ready",[2] = "Marines Fort",[3] = 3}
+local args = {[1] = "Ready",[2] = "Hell City",[3] = 3}
 game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Teleporter"):WaitForChild("MapSelect"):InvokeServer(unpack(args))
 wait(0.1)
-local args = {[1] = "Ready",[2] = "Marines Fort",[3] = 2}
+local args = {[1] = "Ready",[2] = "Hell City",[3] = 2}
 game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Teleporter"):WaitForChild("MapSelect"):InvokeServer(unpack(args))
 wait(0.1)
-local args = {[1] = "Ready",[2] = "Marines Fort",[3] = 1}
+local args = {[1] = "Ready",[2] = "Hell City",[3] = 1}
 game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Teleporter"):WaitForChild("MapSelect"):InvokeServer(unpack(args))
 
 
