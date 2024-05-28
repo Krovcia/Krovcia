d60a4fc30c5d41ecbe2faa944691d3ac84d24579
Krovcia <143706464+Krovcia@users.noreply.github.com>
2024-05-28 14:33:55 +0700

    Create AUTOJOINALSraid1.lua

---
+++ AUTOJOINALSraid1.lua
@@ -0,0 +1,14 @@
+local args = {[1] = "Ready",[2] = "Hell City",[3] = 5}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Teleporter"):WaitForChild("MapSelect"):InvokeServer(unpack(args))
+wait()
+local args = {[1] = "Ready",[2] = "Hell City",[3] = 4}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Teleporter"):WaitForChild("MapSelect"):InvokeServer(unpack(args))
+wait()
+local args = {[1] = "Ready",[2] = "Hell City",[3] = 3}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Teleporter"):WaitForChild("MapSelect"):InvokeServer(unpack(args))
+wait()
+local args = {[1] = "Ready",[2] = "Hell City",[3] = 2}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Teleporter"):WaitForChild("MapSelect"):InvokeServer(unpack(args))
+wait()
+local args = {[1] = "Ready",[2] = "Hell City",[3] = 1}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Teleporter"):WaitForChild("MapSelect"):InvokeServer(unpack(args))
