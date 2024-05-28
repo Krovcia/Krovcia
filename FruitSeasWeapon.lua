e17c43ad22726f0ab9b67ce4e11910a7aa68d794
Krovcia <143706464+Krovcia@users.noreply.github.com>
2024-05-28 17:37:44 +0700

    Create FruitSeasWeapon.lua

---
+++ FruitSeasWeapon.lua
@@ -0,0 +1,65 @@
+local args = {[1] = workspace:WaitForChild("NPC"):WaitForChild("Talk"):WaitForChild("Eddy"):WaitForChild("Info"),[2] = 1,[3] = true}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ToServer"):WaitForChild("SwordR"):InvokeServer(unpack(args))
+wait()
+local args = {[1] = workspace:WaitForChild("NPC"):WaitForChild("Talk"):WaitForChild("Eddy"):WaitForChild("Info"),[2] = 2,[3] = true}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ToServer"):WaitForChild("SwordR"):InvokeServer(unpack(args))
+wait()
+local args = {[1] = workspace:WaitForChild("NPC"):WaitForChild("Talk"):WaitForChild("Eddy"):WaitForChild("Info"),[2] = 3,[3] = true}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ToServer"):WaitForChild("AccessoryR2"):InvokeServer(unpack(args))
+wait()
+local args = {[1] = workspace:WaitForChild("NPC"):WaitForChild("Talk"):WaitForChild("Weak Magician"):WaitForChild("Info"),[2] = 1,[3] = true}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ToServer"):WaitForChild("SwordR"):InvokeServer(unpack(args))
+wait()
+local args = {[1] = workspace:WaitForChild("NPC"):WaitForChild("Talk"):WaitForChild("Sword Collector"):WaitForChild("Info"),[2] = 1,[3] = true}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ToServer"):WaitForChild("SwordR"):InvokeServer(unpack(args))
+wait()
+local args = {[1] = workspace:WaitForChild("NPC"):WaitForChild("Talk"):WaitForChild("Sword Collector"):WaitForChild("Info"),[2] = 2,[3] = true}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ToServer"):WaitForChild("SwordR"):InvokeServer(unpack(args))
+wait()
+local args = {[1] = workspace:WaitForChild("NPC"):WaitForChild("Talk"):WaitForChild("Sword Collector"):WaitForChild("Info"),[2] = 3,[3] = true}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ToServer"):WaitForChild("SwordR"):InvokeServer(unpack(args))
+wait()
+local args = {[1] = workspace:WaitForChild("NPC"):WaitForChild("Talk"):WaitForChild("Gun Merchant"):WaitForChild("Info"),[2] = 2,[3] = true}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ToServer"):WaitForChild("SwordR"):InvokeServer(unpack(args))
+wait()
+local args = {[1] = workspace:WaitForChild("NPC"):WaitForChild("Talk"):WaitForChild("Gun Merchant"):WaitForChild("Info"),[2] = 3,[3] = true}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ToServer"):WaitForChild("SwordR"):InvokeServer(unpack(args))
+wait()
+local args = {[1] = workspace:WaitForChild("NPC"):WaitForChild("Talk"):WaitForChild("Merchant Of The East"):WaitForChild("Info"),[2] = 2,[3] = true}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ToServer"):WaitForChild("SwordR"):InvokeServer(unpack(args))
+wait()
+local args = {[1] = workspace:WaitForChild("NPC"):WaitForChild("Talk"):WaitForChild("Merchant Of The East"):WaitForChild("Info"),[2] = 1,[3] = true}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ToServer"):WaitForChild("SwordR"):InvokeServer(unpack(args))
+wait()
+local args = {[1] = workspace:WaitForChild("NPC"):WaitForChild("Talk"):WaitForChild("Retired Pirate"):WaitForChild("Info"),[2] = 1,[3] = true}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ToServer"):WaitForChild("SwordR"):InvokeServer(unpack(args))
+wait()
+local args = {[1] = workspace:WaitForChild("NPC"):WaitForChild("Talk"):WaitForChild("Retired Pirate"):WaitForChild("Info"),[2] = 2,[3] = true}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ToServer"):WaitForChild("AccessoryR2"):InvokeServer(unpack(args))
+wait()
+local args = {[1] = workspace:WaitForChild("NPC"):WaitForChild("Talk"):WaitForChild("Wyper"):WaitForChild("Info"),[2] = 1,[3] = true}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ToServer"):WaitForChild("SwordR"):InvokeServer(unpack(args))
+wait()
+local args = {[1] = workspace:WaitForChild("NPC"):WaitForChild("Talk"):WaitForChild("Fisherman"):WaitForChild("Info"),[2] = 1,[3] = true}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ToServer"):WaitForChild("AccessoryR2"):InvokeServer(unpack(args))
+wait()
+local args = {[1] = workspace:WaitForChild("NPC"):WaitForChild("Talk"):WaitForChild("Fisherman"):WaitForChild("Info"),[2] = 3,[3] = true}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ToServer"):WaitForChild("AccessoryR2"):InvokeServer(unpack(args))
+wait()
+local args = {[1] = workspace:WaitForChild("NPC"):WaitForChild("Talk"):WaitForChild("Merchant Of The West"):WaitForChild("Info"),[2] = 1,[3] = true}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ToServer"):WaitForChild("SwordR"):InvokeServer(unpack(args))
+wait()
+local args = {[1] = workspace:WaitForChild("NPC"):WaitForChild("Talk"):WaitForChild("Anchient Warrior"):WaitForChild("Info"),[2] = 1,[3] = true}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ToServer"):WaitForChild("AccessoryR2"):InvokeServer(unpack(args))
+wait()
+local args = {[1] = workspace:WaitForChild("NPC"):WaitForChild("Talk"):WaitForChild("Anchient Warrior"):WaitForChild("Info"),[2] = 2,[3] = true}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ToServer"):WaitForChild("SwordR"):InvokeServer(unpack(args))
+wait()
+local args = {[1] = workspace:WaitForChild("NPC"):WaitForChild("Talk"):WaitForChild("Dojo Teacher"):WaitForChild("Info"),[2] = 3,[3] = true}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ToServer"):WaitForChild("AccessoryR2"):InvokeServer(unpack(args))
+wait()
+local args = {[1] = workspace:WaitForChild("NPC"):WaitForChild("Talk"):WaitForChild("Dojo Teacher"):WaitForChild("Info"),[2] = 1,[3] = true}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ToServer"):WaitForChild("AccessoryR2"):InvokeServer(unpack(args))
+wait()
+local args = {[1] = workspace:WaitForChild("NPC"):WaitForChild("Talk"):WaitForChild("Dojo Teacher"):WaitForChild("Info"),[2] = 2,[3] = true}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ToServer"):WaitForChild("AccessoryR2"):InvokeServer(unpack(args))
