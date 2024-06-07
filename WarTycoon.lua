22d05a070517c91a7836beb0e4c08109ce7b3226
Krovcia <143706464+Krovcia@users.noreply.github.com>
2024-06-07 19:49:20 +0700

    Update WarTycoon.lua

--- WarTycoon.lua
+++ WarTycoon.lua
@@ -1,89 +1,243 @@
-if game:GetService("Players").LocalPlayer.leaderstats.Team.Value == "Alpha" then
-local Children = game:GetService("Workspace").Tycoon.Tycoons.Alpha.UnpurchasedButtons
-local MoneyColect = game:GetService("Workspace").Tycoon.Tycoons.Alpha.Essentials.CashCollector.CFrame
+while wait(1) do
 
-elseif game:GetService("Players").LocalPlayer.leaderstats.Team.Value == "Bravo" then
-local Children = game:GetService("Workspace").Tycoon.Tycoons.Bravo.UnpurchasedButtons
-local MoneyColect = game:GetService("Workspace").Tycoon.Tycoons.Bravo.Essentials.CashCollector.CFrame
-
-elseif game:GetService("Players").LocalPlayer.leaderstats.Team.Value == "Charlie" then
-local Children = game:GetService("Workspace").Tycoon.Tycoons.Charlie.UnpurchasedButtons
-local MoneyColect = game:GetService("Workspace").Tycoon.Tycoons.Charlie.Essentials.CashCollector.CFrame
-
-elseif game:GetService("Players").LocalPlayer.leaderstats.Team.Value == "Delta" then
-local Children = game:GetService("Workspace").Tycoon.Tycoons.Delta.UnpurchasedButtons
 local MoneyColect = game:GetService("Workspace").Tycoon.Tycoons.Delta.Essentials.CashCollector.CFrame
+local Money = game:GetService("Players").LocalPlayer.leaderstats.Cash.Value
 
-elseif game:GetService("Players").LocalPlayer.leaderstats.Team.Value == "Echo" then
-local Children = game:GetService("Workspace").Tycoon.Tycoons.Echo.UnpurchasedButtons
-local MoneyColect = game:GetService("Workspace").Tycoon.Tycoons.Echo.Essentials.CashCollector.CFrame
 
-elseif game:GetService("Players").LocalPlayer.leaderstats.Team.Value == "Foxtrot" then
-local Children = game:GetService("Workspace").Tycoon.Tycoons.Foxtrot.UnpurchasedButtons
-local MoneyColect = game:GetService("Workspace").Tycoon.Tycoons.Foxtrot.Essentials.CashCollector.CFrame
+-----------------ke Oil 2----------------------
+if game:GetService("Workspace").Tycoon.Tycoons.Delta.PurchasedObjectCount.Value == 0 then
 
-elseif game:GetService("Players").LocalPlayer.leaderstats.Team.Value == "Golf" then
-local Children = game:GetService("Workspace").Tycoon.Tycoons.Golf.UnpurchasedButtons
-local MoneyColect = game:GetService("Workspace").Tycoon.Tycoons.Golf.Essentials.CashCollector.CFrame
-
-elseif game:GetService("Players").LocalPlayer.leaderstats.Team.Value == "Hotel" then
-local Children = game:GetService("Workspace").Tycoon.Tycoons.Hotel.UnpurchasedButtons
-local MoneyColect = game:GetService("Workspace").Tycoon.Tycoons.Hotel.Essentials.CashCollector.CFrame
-
-elseif game:GetService("Players").LocalPlayer.leaderstats.Team.Value == "Kilo" then
-local Children = game:GetService("Workspace").Tycoon.Tycoons.Kilo.UnpurchasedButtons
-local MoneyColect = game:GetService("Workspace").Tycoon.Tycoons.Kilo.Essentials.CashCollector.CFrame
-
-elseif game:GetService("Players").LocalPlayer.leaderstats.Team.Value == "Lima" then
-local Children = game:GetService("Workspace").Tycoon.Tycoons.Lima.UnpurchasedButtons
-local MoneyColect = game:GetService("Workspace").Tycoon.Tycoons.Lima.Essentials.CashCollector.CFrame
-
-elseif game:GetService("Players").LocalPlayer.leaderstats.Team.Value == "Loading" then
-local Children = game:GetService("Workspace").Tycoon.Tycoons.Loading.UnpurchasedButtons
-local MoneyColect = game:GetService("Workspace").Tycoon.Tycoons.Loading.Essentials.CashCollector.CFrame
-
-elseif game:GetService("Players").LocalPlayer.leaderstats.Team.Value == "Omega" then
-local Children = game:GetService("Workspace").Tycoon.Tycoons.Omega.UnpurchasedButtons
-local MoneyColect = game:GetService("Workspace").Tycoon.Tycoons.Omega.Essentials.CashCollector.CFrame
-
-elseif game:GetService("Players").LocalPlayer.leaderstats.Team.Value == "Romeo" then
-local Children = game:GetService("Workspace").Tycoon.Tycoons.Romeo.UnpurchasedButtons
-local MoneyColect = game:GetService("Workspace").Tycoon.Tycoons.Romeo.Essentials.CashCollector.CFrame
-
-elseif game:GetService("Players").LocalPlayer.leaderstats.Team.Value == "Sierra" then
-local Children = game:GetService("Workspace").Tycoon.Tycoons.Sierra.UnpurchasedButtons
-local MoneyColect = game:GetService("Workspace").Tycoon.Tycoons.Sierra.Essentials.CashCollector.CFrame
-
-elseif game:GetService("Players").LocalPlayer.leaderstats.Team.Value == "Tango" then
-local Children = game:GetService("Workspace").Tycoon.Tycoons.Tango.UnpurchasedButtons
-local MoneyColect = game:GetService("Workspace").Tycoon.Tycoons.Tango.Essentials.CashCollector.CFrame
-
-elseif game:GetService("Players").LocalPlayer.leaderstats.Team.Value == "Victor" then
-local Children = game:GetService("Workspace").Tycoon.Tycoons.Victor.UnpurchasedButtons
-local MoneyColect = game:GetService("Workspace").Tycoon.Tycoons.Victor.Essentials.CashCollector.CFrame
-
-elseif game:GetService("Players").LocalPlayer.leaderstats.Team.Value == "Zulu" then
-local Children = game:GetService("Workspace").Tycoon.Tycoons.Zulu.UnpurchasedButtons
-local MoneyColect = game:GetService("Workspace").Tycoon.Tycoons.Zulu.Essentials.CashCollector.CFrame
-
-end
-
-local Children2 = Children::GetChildren()
-for i = 1, #Children2 do
-if Children[i].Price.Value < game:GetService("Players").LocalPlayer.leaderstats.Cash.Value and Children[i].Price.Value > 0 then
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Children[i].Part.CFrame
+repeat 
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Tycoon.Tycoons.Delta.PurchasedObjects["Oil 1"].Part.CFrame
+wait(0.5)
+keypress(0x45)
 wait(1)
-game.Players.LocalPlayer.Character:FindFirstChildWhichIsA("Humanoid"):ChangeState(Enum.HumanoidStateType.Jumping)
-wait(1)
-else
+keyrelease(0x45)
+wait(2)
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = MoneyColect
+wait(1)
+until game:GetService("Workspace").Tycoon.Tycoons.Delta.UnpurchasedButtons["Oil 2"].Price.Value < game:GetService("Players").LocalPlayer.leaderstats.Cash.Value
+
+repeat 
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Tycoon.Tycoons.Delta.UnpurchasedButtons["Oil 2"].Part.CFrame
+wait(1)
+keypress(0x20)
+wait(1)
+keyrelease(0x20)
+wait(1)
+untilgame:GetService("Workspace").Tycoon.Tycoons.Delta.PurchasedObjectCount.Value == 1
 end
+
+-----------------ke Oil 3----------------------
+if game:GetService("Workspace").Tycoon.Tycoons.Delta.PurchasedObjectCount.Value == 1 then
+
+repeat
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = MoneyColect
+wait(1)
+until game:GetService("Workspace").Tycoon.Tycoons.Delta.UnpurchasedButtons["Oil 3"].Price.Value < game:GetService("Players").LocalPlayer.leaderstats.Cash.Value
+
+repeat 
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Tycoon.Tycoons.Delta.UnpurchasedButtons["Oil 3"].Part.CFrame
+wait(2)
+wait(1)
+keypress(0x20)
+wait(1)
+keyrelease(0x20)
+wait(1)
+untilgame:GetService("Workspace").Tycoon.Tycoons.Delta.PurchasedObjectCount.Value == 2
 end
+
+
+-----------------ke Oil 4----------------------
+if game:GetService("Workspace").Tycoon.Tycoons.Delta.PurchasedObjectCount.Value == 2 then
+
+repeat
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = MoneyColect
+wait(1)
+until game:GetService("Workspace").Tycoon.Tycoons.Delta.UnpurchasedButtons["Oil 4"].Price.Value < game:GetService("Players").LocalPlayer.leaderstats.Cash.Value
+
+repeat 
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Tycoon.Tycoons.Delta.UnpurchasedButtons["Oil 4"].Part.CFrame
+wait(2)
+wait(1)
+keypress(0x20)
+wait(1)
+keyrelease(0x20)
+wait(1)
+untilgame:GetService("Workspace").Tycoon.Tycoons.Delta.PurchasedObjectCount.Value == 3
+end
+
+-----------------ke Oil 5----------------------
+if game:GetService("Workspace").Tycoon.Tycoons.Delta.PurchasedObjectCount.Value == 3 then
+
+repeat
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = MoneyColect
+wait(1)
+until game:GetService("Workspace").Tycoon.Tycoons.Delta.UnpurchasedButtons["Oil 5"].Price.Value < game:GetService("Players").LocalPlayer.leaderstats.Cash.Value
+
+repeat
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Tycoon.Tycoons.Delta.UnpurchasedButtons["Oil 5"].Part.CFrame
+wait(2)
+wait(1)
+keypress(0x20)
+wait(1)
+keyrelease(0x20)
+wait(1)
+until game:GetService("Workspace").Tycoon.Tycoons.Delta.PurchasedObjectCount.Value == 4
+end
+
+-----------------ke Oil 6----------------------
+if game:GetService("Workspace").Tycoon.Tycoons.Delta.PurchasedObjectCount.Value == 4 then
+
+repeat
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = MoneyColect
+wait(1)
+until game:GetService("Workspace").Tycoon.Tycoons.Delta.UnpurchasedButtons["Oil 6"].Price.Value < game:GetService("Players").LocalPlayer.leaderstats.Cash.Value
+
+repeat
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Tycoon.Tycoons.Delta.UnpurchasedButtons["Oil 6"].Part.CFrame
+wait(2)
+wait(1)
+keypress(0x20)
+wait(1)
+keyrelease(0x20)
+wait(1)
+until game:GetService("Workspace").Tycoon.Tycoons.Delta.PurchasedObjectCount.Value == 5
+end
+
+-----------------ke Oil 7(Largeoil 1)----------------------
+if game:GetService("Workspace").Tycoon.Tycoons.Delta.PurchasedObjectCount.Value == 5 then
+
+repeat
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = MoneyColect
+wait(1)
+until game:GetService("Workspace").Tycoon.Tycoons.Delta.UnpurchasedButtons["Large Oil 1"].Price.Value < game:GetService("Players").LocalPlayer.leaderstats.Cash.Value
+
+repeat
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Tycoon.Tycoons.Delta.UnpurchasedButtons["Large Oil 1"].Part.CFrame
+wait(2)
+wait(1)
+keypress(0x20)
+wait(1)
+keyrelease(0x20)
+wait(1)
+until game:GetService("Workspace").Tycoon.Tycoons.Delta.PurchasedObjectCount.Value == 6
+end
+
+-----------------ke Oil 8(Largeoil 2)----------------------
+if game:GetService("Workspace").Tycoon.Tycoons.Delta.PurchasedObjectCount.Value == 6 then
+
+repeat
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = MoneyColect
+wait(1)
+until game:GetService("Workspace").Tycoon.Tycoons.Delta.UnpurchasedButtons["Large Oil 2"].Price.Value < game:GetService("Players").LocalPlayer.leaderstats.Cash.Value
+
+repeat
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Tycoon.Tycoons.Delta.UnpurchasedButtons["Large Oil 2"].Part.CFrame
+wait(2)
+wait(1)
+keypress(0x20)
+wait(1)
+keyrelease(0x20)
+wait(1)
+until game:GetService("Workspace").Tycoon.Tycoons.Delta.PurchasedObjectCount.Value == 7
+end
+
+-----------------ke Oil 9(Largeoil 3)----------------------
+if game:GetService("Workspace").Tycoon.Tycoons.Delta.PurchasedObjectCount.Value == 7 then
+
+repeat
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = MoneyColect
+wait(1)
+until game:GetService("Workspace").Tycoon.Tycoons.Delta.UnpurchasedButtons["Large Oil 3"].Price.Value < game:GetService("Players").LocalPlayer.leaderstats.Cash.Value
+
+repeat
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Tycoon.Tycoons.Delta.UnpurchasedButtons["Large Oil 3"].Part.CFrame
+wait(2)
+wait(1)
+keypress(0x20)
+wait(1)
+keyrelease(0x20)
+wait(1)
+until game:GetService("Workspace").Tycoon.Tycoons.Delta.PurchasedObjectCount.Value == 8
+end
+
+-----------------ke Oil 10(Largeoil 4)----------------------
+if game:GetService("Workspace").Tycoon.Tycoons.Delta.PurchasedObjectCount.Value == 8 then
+
+repeat
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = MoneyColect
+wait(1)
+until game:GetService("Workspace").Tycoon.Tycoons.Delta.UnpurchasedButtons["Large Oil 4"].Price.Value < game:GetService("Players").LocalPlayer.leaderstats.Cash.Value
+
+repeat
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Tycoon.Tycoons.Delta.UnpurchasedButtons["Large Oil 4"].Part.CFrame
+wait(2)
+wait(1)
+keypress(0x20)
+wait(1)
+keyrelease(0x20)
+wait(1)
+until game:GetService("Workspace").Tycoon.Tycoons.Delta.PurchasedObjectCount.Value == 9
+end
+
+-----------------ke Oil 11(Largeoil 5)----------------------
+if game:GetService("Workspace").Tycoon.Tycoons.Delta.PurchasedObjectCount.Value == 9 then
+
+repeat
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = MoneyColect
+wait(1)
+until game:GetService("Workspace").Tycoon.Tycoons.Delta.UnpurchasedButtons["Large Oil 5"].Price.Value < game:GetService("Players").LocalPlayer.leaderstats.Cash.Value
+
+repeat
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Tycoon.Tycoons.Delta.UnpurchasedButtons["Large Oil 5"].Part.CFrame
+wait(2)
+wait(1)
+keypress(0x20)
+wait(1)
+keyrelease(0x20)
+wait(1)
+until game:GetService("Workspace").Tycoon.Tycoons.Delta.PurchasedObjectCount.Value == 10
+end
+
+-----------------ke Oil 12(Largeoil 6)----------------------
+if game:GetService("Workspace").Tycoon.Tycoons.Delta.PurchasedObjectCount.Value == 10 then
+
+repeat
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = MoneyColect
+wait(1)
+until game:GetService("Workspace").Tycoon.Tycoons.Delta.UnpurchasedButtons["Large Oil 6"].Price.Value < game:GetService("Players").LocalPlayer.leaderstats.Cash.Value
+
+repeat
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Tycoon.Tycoons.Delta.UnpurchasedButtons["Large Oil 6"].Part.CFrame
+wait(2)
+wait(1)
+keypress(0x20)
+wait(1)
+keyrelease(0x20)
+wait(1)
+until game:GetService("Workspace").Tycoon.Tycoons.Delta.PurchasedObjectCount.Value == 11
+end
+
+--FARM--
+if game:GetService("Workspace").Tycoon.Tycoons.Delta.PurchasedObjectCount.Value == 11 then
+
+local Team = game:GetService("Workspace").Tycoon.Tycoons.Delta.UnpurchasedButtons
+local Children = Team:GetChildren()
 
 for i = 1, #Children do
-if Children[i].Neon.BrickColor == BrickColor.new("New Yeller") then
+if Children[i].Neon.BrickColor == BrickColor.new("New Yeller") or Children[i].Neon.BrickColor == BrickColor.new("Lime green") or Children[i].Neon.BrickColor == BrickColor.new("Cyan") then
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Children[i].Part.CFrame
+wait()
+keypress(0x45)
+keypress(0x20)
+wait(0.5)
+keyrelease(0x45)
+keyrelease(0x20)
 wait(1)
-else
 end
 end
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = MoneyColect
+end
+
+    end
