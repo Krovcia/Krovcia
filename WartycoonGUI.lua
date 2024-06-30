5e96e2b548cf6ea2a47de30a7179e4f6e8efbec8
Krovcia <143706464+Krovcia@users.noreply.github.com>
2024-07-01 02:17:58 +0700

    Create WartycoonGUI.lua

---
+++ WartycoonGUI.lua
@@ -0,0 +1,291 @@
+local gui = Instance.new("ScreenGui")
+gui.Parent = game.Players.LocalPlayer.PlayerGui -- This places the GUI in the player's screen
+
+local Euro = Instance.new("TextButton")
+Euro.Parent = gui
+Euro.Position = UDim2.new(0, 100, 0, 50)
+Euro.Size = UDim2.new(0, 75, 0, 20)
+Euro.TextSize = 8
+Euro.Text = "EuroCopter(H)"
+
+local AC = Instance.new("TextButton")
+AC.Parent = gui
+AC.Position = UDim2.new(0, 100, 0, 70)
+AC.Size = UDim2.new(0, 75, 0, 20)
+AC.TextSize = 8
+AC.Text = "AC-130(G)"
+
+local PL = Instance.new("TextButton")
+PL.Parent = gui
+PL.Position = UDim2.new(0, 100, 0, 90)
+PL.Size = UDim2.new(0, 75, 0, 20)
+PL.TextSize = 8
+PL.Text = "PL-10(T)"
+
+local Lazar = Instance.new("TextButton")
+Lazar.Parent = gui
+Lazar.Position = UDim2.new(0, 100, 0, 115)
+Lazar.Size = UDim2.new(0, 75, 0, 20)
+Lazar.TextSize = 8
+Lazar.Text = "Lazar(G)"
+
+local T72 = Instance.new("TextButton")
+T72.Parent = gui
+T72.Position = UDim2.new(0, 100, 0, 135)
+T72.Size = UDim2.new(0, 75, 0, 20)
+T72.TextSize = 8
+T72.Text = "T-72(T)"
+
+local A10 = Instance.new("TextButton")
+A10.Parent = gui
+A10.Position = UDim2.new(0, 100, 0, 160)
+A10.Size = UDim2.new(0, 75, 0, 20)
+A10.TextSize = 8
+A10.Text = "A10W(P)"
+
+local Patriot = Instance.new("TextButton")
+Patriot.Parent = gui
+Patriot.Position = UDim2.new(0, 100, 0, 185)
+Patriot.Size = UDim2.new(0, 75, 0, 20)
+Patriot.TextSize = 8
+Patriot.Text = "Patriot(G)"
+
+local KA52 = Instance.new("TextButton")
+KA52.Parent = gui
+KA52.Position = UDim2.new(0, 100, 0, 205)
+KA52.Size = UDim2.new(0, 75, 0, 20)
+KA52.TextSize = 8
+KA52.Text = "KA52(H)"
+
+local ADATS = Instance.new("TextButton")
+ADATS.Parent = gui
+ADATS.Position = UDim2.new(0, 100, 0, 225)
+ADATS.Size = UDim2.new(0, 75, 0, 20)
+ADATS.TextSize = 8
+ADATS.Text = "ADATS(T)"
+
+local F16 = Instance.new("TextButton")
+F16.Parent = gui
+F16.Position = UDim2.new(0, 100, 0, 245)
+F16.Size = UDim2.new(0, 75, 0, 20)
+F16.TextSize = 8
+F16.Text = "F16(P)"
+
+local gui = Instance.new("ScreenGui")
+gui.Parent = game.Players.LocalPlayer.PlayerGui -- This places the GUI in the player's screen
+
+
+local MIG29 = Instance.new("TextButton")
+MIG29.Parent = gui
+MIG29.Position = UDim2.new(0, 100, 0, 270)
+MIG29.Size = UDim2.new(0, 75, 0, 20)
+MIG29.TextSize = 8
+MIG29.Text = "MIG29(P)"
+
+local Stallion = Instance.new("TextButton")
+Stallion.Parent = gui
+Stallion.Position = UDim2.new(0, 100, 0, 295)
+Stallion.Size = UDim2.new(0, 75, 0, 20)
+Stallion.TextSize = 8
+Stallion.Text = "Stallion(H)"
+
+local USS = Instance.new("TextButton")
+USS.Parent = gui
+USS.Position = UDim2.new(0, 100, 0, 315)
+USS.Size = UDim2.new(0, 75, 0, 20)
+USS.TextSize = 8
+USS.Text = "USS(B)"
+
+local A10A = Instance.new("TextButton")
+A10A.Parent = gui
+A10A.Position = UDim2.new(0, 100, 0, 340)
+A10A.Size = UDim2.new(0, 75, 0, 20)
+A10A.TextSize = 8
+A10A.Text = "A10A(G)"
+
+local AH64 = Instance.new("TextButton")
+AH64.Parent = gui
+AH64.Position = UDim2.new(0, 100, 0, 360)
+AH64.Size = UDim2.new(0, 75, 0, 20)
+AH64.TextSize = 8
+AH64.Text = "AH64(H)"
+
+
+
+local gui = Instance.new("ScreenGui")
+gui.Parent = game.Players.LocalPlayer.PlayerGui -- This places the GUI in the player's screen
+
+local Euro1 = Instance.new("TextButton")
+Euro1.Parent = gui
+Euro1.Position = UDim2.new(0, 25, 0, 50)
+Euro1.Size = UDim2.new(0, 75, 0, 20)
+Euro1.TextSize = 8
+Euro1.Text = "D50GWT"
+
+local Lazar1 = Instance.new("TextButton")
+Lazar1.Parent = gui
+Lazar1.Position = UDim2.new(0, 25, 0, 115)
+Lazar1.Size = UDim2.new(0, 75, 0, 20)
+Lazar1.TextSize = 8
+Lazar1.Text = "K40WT"
+
+local A101 = Instance.new("TextButton")
+A101.Parent = gui
+A101.Position = UDim2.new(0, 25, 0, 160)
+A101.Size = UDim2.new(0, 75, 0, 20)
+A101.TextSize = 8
+A101.Text = "D50GWP"
+
+local Patriot1 = Instance.new("TextButton")
+Patriot1.Parent = gui
+Patriot1.Position = UDim2.new(0, 25, 0, 185)
+Patriot1.Size = UDim2.new(0, 75, 0, 20)
+Patriot1.TextSize = 8
+Patriot1.Text = "D45HWP"
+
+local MIG291 = Instance.new("TextButton")
+MIG291.Parent = gui
+MIG291.Position = UDim2.new(0, 25, 0, 270)
+MIG291.Size = UDim2.new(0, 75, 0, 20)
+MIG291.TextSize = 8
+MIG291.Text = "K45WP"
+
+local Stallion1 = Instance.new("TextButton")
+Stallion1.Parent = gui
+Stallion1.Position = UDim2.new(0, 25, 0, 295)
+Stallion1.Size = UDim2.new(0, 75, 0, 20)
+Stallion1.TextSize = 8
+Stallion1.Text = "D35BWH"
+
+local A10A1 = Instance.new("TextButton")
+A10A1.Parent = gui
+A10A1.Position = UDim2.new(0, 25, 0, 340)
+A10A1.Size = UDim2.new(0, 75, 0, 20)
+A10A1.TextSize = 8
+A10A1.Text = "D40GWH"
+
+
+while wait(3) do
+
+local gui = Instance.new("ScreenGui")
+gui.Parent = game.Players.LocalPlayer.PlayerGui -- This places the GUI in the player's screen
+
+local EuroP = Instance.new("TextButton")
+EuroP.Parent = gui
+EuroP.Position = UDim2.new(0, 175, 0, 50)
+EuroP.Size = UDim2.new(0, 30, 0, 20)
+EuroP.TextSize = 8
+EuroP.Text = workspace.Tycoon.Tycoons.Alpha.Operations["Operation Ground Strike"].Value
+
+local ACP = Instance.new("TextButton")
+ACP.Parent = gui
+ACP.Position = UDim2.new(0, 175, 0, 70)
+ACP.Size = UDim2.new(0, 30, 0, 20)
+ACP.TextSize = 8
+ACP.Text = workspace.Tycoon.Tycoons.Alpha.Operations["Operation Firepower"].Value
+
+local PLP = Instance.new("TextButton")
+PLP.Parent = gui
+PLP.Position = UDim2.new(0, 175, 0, 90)
+PLP.Size = UDim2.new(0, 30, 0, 20)
+PLP.TextSize = 8
+PLP.Text = workspace.Tycoon.Tycoons.Alpha.Operations["Operation Ground Dominance"].Value
+
+local LazarP = Instance.new("TextButton")
+LazarP.Parent = gui
+LazarP.Position = UDim2.new(0, 175, 0, 115)
+LazarP.Size = UDim2.new(0, 30, 0, 20)
+LazarP.TextSize = 8
+LazarP.Text = workspace.Tycoon.Tycoons.Alpha.Operations["Operation Takedown"].Value
+
+local T72P = Instance.new("TextButton")
+T72P.Parent = gui
+T72P.Position = UDim2.new(0, 175, 0, 135)
+T72P.Size = UDim2.new(0, 30, 0, 20)
+T72P.TextSize = 8
+T72P.Text = workspace.Tycoon.Tycoons.Alpha.Operations["Operation Armored Assault"].Value
+
+local A10P = Instance.new("TextButton")
+A10P.Parent = gui
+A10P.Position = UDim2.new(0, 175, 0, 160)
+A10P.Size = UDim2.new(0, 30, 0, 20)
+A10P.TextSize = 8
+A10P.Text = workspace.Tycoon.Tycoons.Alpha.Operations["Operation Air Support"].Value
+
+local PatriotP = Instance.new("TextButton")
+PatriotP.Parent = gui
+PatriotP.Position = UDim2.new(0, 175, 0, 185)
+PatriotP.Size = UDim2.new(0, 30, 0, 20)
+PatriotP.TextSize = 8
+PatriotP.Text = workspace.Tycoon.Tycoons.Alpha.Operations["Operation Sky Sentinel"].Value
+
+local gui = Instance.new("ScreenGui")
+gui.Parent = game.Players.LocalPlayer.PlayerGui -- This places the GUI in the player's screen
+
+local KA52P = Instance.new("TextButton")
+KA52P.Parent = gui
+KA52P.Position = UDim2.new(0, 175, 0, 205)
+KA52P.Size = UDim2.new(0, 30, 0, 20)
+KA52P.TextSize = 8
+KA52P.Text = workspace.Tycoon.Tycoons.Alpha.Operations["Operation Havoc"].Value
+
+local ADATSP = Instance.new("TextButton")
+ADATSP.Parent = gui
+ADATSP.Position = UDim2.new(0, 175, 0, 225)
+ADATSP.Size = UDim2.new(0, 30, 0, 20)
+ADATSP.TextSize = 8
+ADATSP.Text = workspace.Tycoon.Tycoons.Alpha.Operations["Operation Sky Shield"].Value
+
+local F16P = Instance.new("TextButton")
+F16P.Parent = gui
+F16P.Position = UDim2.new(0, 175, 0, 245)
+F16P.Size = UDim2.new(0, 30, 0, 20)
+F16P.TextSize = 8
+F16P.Text = workspace.Tycoon.Tycoons.Alpha.Operations["Operation Sky Hunter"].Value
+
+local MIG29P = Instance.new("TextButton")
+MIG29P.Parent = gui
+MIG29P.Position = UDim2.new(0, 175, 0, 270)
+MIG29P.Size = UDim2.new(0, 30, 0, 20)
+MIG29P.TextSize = 8
+MIG29P.Text = workspace.Tycoon.Tycoons.Alpha.Operations["Operation Aerial Ace"].Value
+
+local StallionP = Instance.new("TextButton")
+StallionP.Parent = gui
+StallionP.Position = UDim2.new(0, 175, 0, 295)
+StallionP.Size = UDim2.new(0, 30, 0, 20)
+StallionP.TextSize = 8
+StallionP.Text = workspace.Tycoon.Tycoons.Alpha.Operations["Operation Sea Fury"].Value
+
+local USSP = Instance.new("TextButton")
+USSP.Parent = gui
+USSP.Position = UDim2.new(0, 175, 0, 315)
+USSP.Size = UDim2.new(0, 30, 0, 20)
+USSP.TextSize = 8
+USSP.Text = workspace.Tycoon.Tycoons.Alpha.Operations["Operation Sea Crusher"].Value
+
+local A10AP = Instance.new("TextButton")
+A10AP.Parent = gui
+A10AP.Position = UDim2.new(0, 175, 0, 340)
+A10AP.Size = UDim2.new(0, 30, 0, 20)
+A10AP.TextSize = 8
+A10AP.Text = workspace.Tycoon.Tycoons.Alpha.Operations["Operation Blowup"].Value
+
+local AH64P = Instance.new("TextButton")
+AH64P.Parent = gui
+AH64P.Position = UDim2.new(0, 175, 0, 360)
+AH64P.Size = UDim2.new(0, 30, 0, 20)
+AH64P.TextSize = 8
+AH64P.Text = workspace.Tycoon.Tycoons.Alpha.Operations["Operation Chopper"].Value
+
+
+
+
+
+
+
+
+
+
+
+
