281946c85c1d0f95e8d2e75f333744d75a2ed624
Krovcia <143706464+Krovcia@users.noreply.github.com>
2024-07-04 01:41:21 +0700

    Update WartycoonOPE.lua

--- WartycoonOPE.lua
+++ WartycoonOPE.lua
@@ -1,1 +1,28 @@
-a
+repeat wait() until game:IsLoaded()
+wait()
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local Window = Rayfield:CreateWindow({
+   Name = "Rayfield Example Window",
+   LoadingTitle = "Rayfield Interface Suite",
+   LoadingSubtitle = "by Sirius",
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = nil, -- Create a custom folder for your hub/game
+      FileName = "Big Hub"
+   }
+})
+local Tab = Window:CreateTab("Tab Example", 4483362458) -- Title, Image
+
+local Button = Tab:CreateButton({
+   Name = "GUI",
+   Callback = function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Krovcia/Krovcia/main/WartycoonGUI.lua"))()
+   end,
+})
+
+local Button = Tab:CreateButton({
+   Name = "GUI",
+   Callback = function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Krovcia/Krovcia/main/WartycoonGUI.lua"))()
+   end,
+})
