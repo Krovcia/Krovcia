443332fc7bbb75eddf4350f0c7f9b2e3c006542d
Krovcia <143706464+Krovcia@users.noreply.github.com>
2024-07-19 16:13:15 +0700

    Create KLGUI.lua

---
+++ KLGUI.lua
@@ -0,0 +1,231 @@
+local gui = Instance.new("ScreenGui")
+gui.Parent = game.Players.LocalPlayer.PlayerGui -- This places the GUI in the player's screen
+
+local Muramasa = Instance.new("TextButton")
+Muramasa.Parent = gui
+Muramasa.Position = UDim2.new(0, 10, 0, 130)
+Muramasa.Size = UDim2.new(0, 75, 0, 20)
+Muramasa.TextSize = 8
+Muramasa.Text = "Muramasa"
+
+local Acroscythe = Instance.new("TextButton")
+Acroscythe.Parent = gui
+Acroscythe.Position = UDim2.new(0, 10, 0, 150)
+Acroscythe.Size = UDim2.new(0, 75, 0, 20)
+Acroscythe.TextSize = 8
+Acroscythe.Text = "Acroscythe"
+
+local Longaevus = Instance.new("TextButton")
+Longaevus.Parent = gui
+Longaevus.Position = UDim2.new(0, 10, 0, 175)
+Longaevus.Size = UDim2.new(0, 75, 0, 20)
+Longaevus.TextSize = 8
+Longaevus.Text = "Longaevus"
+
+local ATK = Instance.new("TextButton")
+ATK.Parent = gui
+ATK.Position = UDim2.new(0, 10, 0, 195)
+ATK.Size = UDim2.new(0, 75, 0, 20)
+ATK.TextSize = 8
+ATK.Text = "ATK"
+
+local Saber = Instance.new("TextButton")
+Saber.Parent = gui
+Saber.Position = UDim2.new(0, 10, 0, 215)
+Saber.Size = UDim2.new(0, 75, 0, 20)
+Saber.TextSize = 8
+Saber.Text = "Saber"
+
+local Avalon = Instance.new("TextButton")
+Avalon.Parent = gui
+Avalon.Position = UDim2.new(0, 10, 0, 235)
+Avalon.Size = UDim2.new(0, 75, 0, 20)
+Avalon.TextSize = 8
+Avalon.Text = "Avalon"
+
+local DStandard = Instance.new("TextButton")
+DStandard.Parent = gui
+DStandard.Position = UDim2.new(0, 10, 0, 260)
+DStandard.Size = UDim2.new(0, 75, 0, 20)
+DStandard.TextSize = 8
+DStandard.Text = "DStandard"
+
+local PhoenixB = Instance.new("TextButton")
+PhoenixB.Parent = gui
+PhoenixB.Position = UDim2.new(0, 10, 0, 280)
+PhoenixB.Size = UDim2.new(0, 75, 0, 20)
+PhoenixB.TextSize = 8
+PhoenixB.Text = "PhoenixB"
+
+local HellSword = Instance.new("TextButton")
+HellSword.Parent = gui
+HellSword.Position = UDim2.new(0, 10, 0, 300)
+HellSword.Size = UDim2.new(0, 75, 0, 20)
+HellSword.TextSize = 8
+HellSword.Text = "HellSword"
+
+local CookieSword = Instance.new("TextButton")
+CookieSword.Parent = gui
+CookieSword.Position = UDim2.new(0, 10, 0, 325)
+CookieSword.Size = UDim2.new(0, 75, 0, 20)
+CookieSword.TextSize = 8
+CookieSword.Text = "CookieSword"
+
+local BShoulder = Instance.new("TextButton")
+BShoulder.Parent = gui
+BShoulder.Position = UDim2.new(0, 10, 0, 345)
+BShoulder.Size = UDim2.new(0, 75, 0, 20)
+BShoulder.TextSize = 8
+BShoulder.Text = "BShoulder"
+
+local MetalTrident = Instance.new("TextButton")
+MetalTrident.Parent = gui
+MetalTrident.Position = UDim2.new(0, 10, 0, 370)
+MetalTrident.Size = UDim2.new(0, 75, 0, 20)
+MetalTrident.TextSize = 8
+MetalTrident.Text = "MetalTrident"
+
+local BlueScarf = Instance.new("TextButton")
+BlueScarf.Parent = gui
+BlueScarf.Position = UDim2.new(0, 10, 0, 390)
+BlueScarf.Size = UDim2.new(0, 75, 0, 20)
+BlueScarf.TextSize = 8
+BlueScarf.Text = "BlueScarf"
+
+local SoulCane = Instance.new("TextButton")
+SoulCane.Parent = gui
+SoulCane.Position = UDim2.new(0, 10, 0, 415)
+SoulCane.Size = UDim2.new(0, 75, 0, 20)
+SoulCane.TextSize = 8
+SoulCane.Text = "SoulCane"
+
+local SallyCrown = Instance.new("TextButton")
+SallyCrown.Parent = gui
+SallyCrown.Position = UDim2.new(0, 10, 0, 435)
+SallyCrown.Size = UDim2.new(0, 75, 0, 20)
+SallyCrown.TextSize = 8
+SallyCrown.Text = "SallyCrown"
+
+local Apollos = Instance.new("TextButton")
+Apollos.Parent = gui
+Apollos.Position = UDim2.new(0, 90, 0, 130)
+Apollos.Size = UDim2.new(0, 75, 0, 20)
+Apollos.TextSize = 8
+Apollos.Text = "Apollos"
+
+local FloffyG = Instance.new("TextButton")
+FloffyG.Parent = gui
+FloffyG.Position = UDim2.new(0, 90, 0, 150)
+FloffyG.Size = UDim2.new(0, 75, 0, 20)
+FloffyG.TextSize = 8
+FloffyG.Text = "FloffyG"
+
+local Advknife = Instance.new("TextButton")
+Advknife.Parent = gui
+Advknife.Position = UDim2.new(0, 90, 0, 175)
+Advknife.Size = UDim2.new(0, 75, 0, 20)
+Advknife.TextSize = 8
+Advknife.Text = "Advknife"
+
+local Anubis = Instance.new("TextButton")
+Anubis.Parent = gui
+Anubis.Position = UDim2.new(0, 90, 0, 195)
+Anubis.Size = UDim2.new(0, 75, 0, 20)
+Anubis.TextSize = 8
+Anubis.Text = "Anubis"
+
+local SunkenBlade = Instance.new("TextButton")
+SunkenBlade.Parent = gui
+SunkenBlade.Position = UDim2.new(0, 90, 0, 220)
+SunkenBlade.Size = UDim2.new(0, 75, 0, 20)
+SunkenBlade.TextSize = 8
+SunkenBlade.Text = "SunkenBlade"
+
+local PondereB = Instance.new("TextButton")
+PondereB.Parent = gui
+PondereB.Position = UDim2.new(0, 90, 0, 240)
+PondereB.Size = UDim2.new(0, 75, 0, 20)
+PondereB.TextSize = 8
+PondereB.Text = "PondereB"
+
+local PondereC = Instance.new("TextButton")
+PondereC.Parent = gui
+PondereC.Position = UDim2.new(0, 90, 0, 260)
+PondereC.Size = UDim2.new(0, 75, 0, 20)
+PondereC.TextSize = 8
+PondereC.Text = "PondereC"
+
+local TenguMask = Instance.new("TextButton")
+TenguMask.Parent = gui
+TenguMask.Position = UDim2.new(0, 90, 0, 285)
+TenguMask.Size = UDim2.new(0, 75, 0, 20)
+TenguMask.TextSize = 8
+TenguMask.Text = "TenguMask"
+
+local OniMask = Instance.new("TextButton")
+OniMask.Parent = gui
+OniMask.Position = UDim2.new(0, 90, 0, 305)
+OniMask.Size = UDim2.new(0, 75, 0, 20)
+OniMask.TextSize = 8
+OniMask.Text = "OniMask"
+
+local Lucidus = Instance.new("TextButton")
+Lucidus.Parent = gui
+Lucidus.Position = UDim2.new(0, 90, 0, 325)
+Lucidus.Size = UDim2.new(0, 75, 0, 20)
+Lucidus.TextSize = 8
+Lucidus.Text = "Lucidus"
+
+for _, sword in pairs(game:GetService("Players").LocalPlayer.Inventory:GetChildren()) do
+if sword = "Muramasa" then
+Muramasa.BackgroundColor3 = Color3.fromRGB(0, 200, 0)
+end
+if sword = "Acroscythe" then
+Acroscythe.BackgroundColor3 = Color3.fromRGB(0, 200, 0)
+end
+if sword = "Longaevus" then
+Longaevus.BackgroundColor3 = Color3.fromRGB(0, 200, 0)
+end
+if sword = "Saber" then
+Saber.BackgroundColor3 = Color3.fromRGB(0, 200, 0)
+end
+if sword = "DStandard" then
+DStandard.BackgroundColor3 = Color3.fromRGB(0, 200, 0)
+end
+if sword = "PhoenixB" then
+PhoenixB.BackgroundColor3 = Color3.fromRGB(0, 200, 0)
+end
+if sword = "HellSword" then
+HellSword.BackgroundColor3 = Color3.fromRGB(0, 200, 0)
+end
+if sword = "CookieSword" then
+CookieSword.BackgroundColor3 = Color3.fromRGB(0, 200, 0)
+end
+if sword = "MetalTrident" then
+MetalTrident.BackgroundColor3 = Color3.fromRGB(0, 200, 0)
+end
+if sword = "SoulCane" then
+SoulCane.BackgroundColor3 = Color3.fromRGB(0, 200, 0)
+end
+if sword = "Apollos" then
+Apollos.BackgroundColor3 = Color3.fromRGB(0, 200, 0)
+end
+if sword = "Advknife" then
+Advknife.BackgroundColor3 = Color3.fromRGB(0, 200, 0)
+end
+if sword = "Anubis" then
+Anubis.BackgroundColor3 = Color3.fromRGB(0, 200, 0)
+end
+if sword = "SunkenBlade" then
+SunkenBlade.BackgroundColor3 = Color3.fromRGB(0, 200, 0)
+end
+if sword = "PondereB" then
+PondereB.BackgroundColor3 = Color3.fromRGB(0, 200, 0)
+end
+if sword = "ATK" then
+ATK.BackgroundColor3 = Color3.fromRGB(0, 200, 0)
+end
+if sword = "Avalon" then
+Avalon.BackgroundColor3 = Color3.fromRGB(0, 200, 0)
+end
+    end
