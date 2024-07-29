d7638676b878463954a7d309cfede1fa1dabaee1
Krovcia <143706464+Krovcia@users.noreply.github.com>
2024-07-29 12:27:53 +0700

    Update TEST62.lua

--- TEST62.lua
+++ TEST62.lua
@@ -41,7 +41,7 @@ Servertime.Position = UDim2.new(0, 400, 0, 0)
 Servertime.TextSize = 10
 Servertime.Size = UDim2.new(0, 150, 0, 20)
 
-while wait(1) do
+while wait(5) do
 
 if game:GetService("Players").LocalPlayer.Character == nil then
     elseif game:GetService("Players").LocalPlayer.PlayerGui.UI.HUD.Visible == true then
