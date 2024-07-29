bcb4688f224327d201cfa919a85af4aeed07ffb3
Krovcia <143706464+Krovcia@users.noreply.github.com>
2024-07-29 16:53:08 +0700

    Update WartycoonGUI.lua

--- WartycoonGUI.lua
+++ WartycoonGUI.lua
@@ -317,10 +317,30 @@ Pantsir1.Size = UDim2.new(0, 30, 0, 20)
 Pantsir1.TextSize = 8
 Pantsir1.Text = workspace.Tycoon.Tycoons.Alpha.Operations.ScavengeOperations["Operation Air Defense"].Value
 
+local Invi = Instance.new("TextButton")
+Invi.Parent = gui
+Invi.Position = UDim2.new(0, 175, 0, 385)
+Invi.Size = UDim2.new(0, 30, 0, 20)
+Invi.TextSize = 8
+Invi.Text = workspace.Tycoon.Tycoons.Alpha.Operations["Operation Rotor Fury"].Value
+
+local Invi = Instance.new("TextButton")
+Invi.Parent = gui
+Invi.Position = UDim2.new(0, 100, 0, 385)
+Invi.Size = UDim2.new(0, 75, 0, 20)
+Invi.TextSize = 8
+Invi.Text = "Invictus(H)"
+
 if workspace.Tycoon.Tycoons.Alpha.Operations["Operation Ground Strike"].Value == workspace.Tycoon.Tycoons.Alpha.Operations["Operation Ground Strike"]:GetAttribute("RequiredAmount") then
 EuroP.BackgroundColor3 = Color3.fromRGB(0, 200, 0)
 else
 EuroP.BackgroundColor3 = Color3.fromRGB(200, 0, 0)
+end
+
+if workspace.Tycoon.Tycoons.Alpha.Operations["Operation Rotor Fury"].Value == workspace.Tycoon.Tycoons.Alpha.Operations["Operation Rotor Fury"]:GetAttribute("RequiredAmount") then
+Invi.BackgroundColor3 = Color3.fromRGB(0, 200, 0)
+else
+Invi.BackgroundColor3 = Color3.fromRGB(200, 0, 0)
 end
 
 if workspace.Tycoon.Tycoons.Alpha.Operations["Operation Firepower"].Value == workspace.Tycoon.Tycoons.Alpha.Operations["Operation Firepower"]:GetAttribute("RequiredAmount") then
