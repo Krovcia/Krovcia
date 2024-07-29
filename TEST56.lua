f2656c05dc9aa4601b1164c0a0ddfa026cb8a1ca
Krovcia <143706464+Krovcia@users.noreply.github.com>
2024-07-29 22:49:17 +0700

    Update TEST56.lua

--- TEST56.lua
+++ TEST56.lua
@@ -16,6 +16,10 @@ game.StarterGui:SetCoreGuiEnabled(2, true)
         --Activate Skill--
         VirtualInputManager:SendMouseButtonEvent(500, 400, 0, true, game, 1)
         VirtualInputManager:SendMouseButtonEvent(500, 400, 0, false, game, 1)
+        wait(0.1)
+        --Activate Skill--
+        VirtualInputManager:SendMouseButtonEvent(500, 400, 0, true, game, 1)
+        VirtualInputManager:SendMouseButtonEvent(500, 400, 0, false, game, 1)
         ------------------
         wait(0.1)
         --Keypress 2--
