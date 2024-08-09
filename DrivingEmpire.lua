54fc215be7c92fa47ec61742a8af078be28266b8
Krovcia <143706464+Krovcia@users.noreply.github.com>
2024-08-10 02:15:09 +0700

    Create DrivingEmpire.lua

---
+++ DrivingEmpire.lua
@@ -0,0 +1,201 @@
+wait(60)
+
+local args = {
+    [1] = {
+        [1] = "ApolloS",
+        [2] = Color3.new(1, 0.4000000059604645, 0),
+        [3] = Color3.new(1, 1, 1),
+        [4] = Color3.new(0.06666667014360428, 0.06666667014360428, 0.06666667014360428)
+    },
+    [2] = "playerActionController"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Purchase"):InvokeServer(unpack(args))
+wait(0.5)
+local args = {
+    [1] = {
+        [1] = "Apollo-Sport",
+        [2] = Color3.new(0.08627451211214066, 0.2823529541492462, 0.6313725709915161),
+        [3] = Color3.new(1, 1, 1),
+        [4] = Color3.new(0.6392157077789307, 0.6352941393852234, 0.6470588445663452)
+    },
+    [2] = "playerActionController"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Purchase"):InvokeServer(unpack(args))
+wait(1)
+local args = {
+    [1] = "ApolloCollections",
+    [2] = "ApolloStartersI"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("AttemptClaimSubCollectionRewardRemoteFunction"):InvokeServer(unpack(args))
+
+
+local args = {
+    [1] = {
+        [1] = "Audi-RS7Sportback",
+        [2] = Color3.new(0.5921568870544434, 0, 0),
+        [3] = Color3.new(1, 1, 1),
+        [4] = Color3.new(0.06666667014360428, 0.06666667014360428, 0.06666667014360428)
+    },
+    [2] = "playerActionController"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Purchase"):InvokeServer(unpack(args))
+
+local args = {
+    [1] = {
+        [1] = "Audi-A4",
+        [2] = Color3.new(0.9725490212440491, 0.9725490212440491, 0.9725490212440491),
+        [3] = Color3.new(1, 1, 1),
+        [4] = Color3.new(0.7921568751335144, 0.7960784435272217, 0.8196078538894653)
+    },
+    [2] = "playerActionController"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Purchase"):InvokeServer(unpack(args))
+
+local args = {
+    [1] = {
+        [1] = "Audi-A6",
+        [2] = Color3.new(0.08235294371843338, 0.13333334028720856, 0.24705882370471954),
+        [3] = Color3.new(1, 1, 1),
+        [4] = Color3.new(0.6392157077789307, 0.6352941393852234, 0.6470588445663452)
+    },
+    [2] = "playerActionController"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Purchase"):InvokeServer(unpack(args))
+
+local args = {
+    [1] = {
+        [1] = "T4",
+        [2] = Color3.new(0.12941177189350128, 0.26274511218070984, 0.4627451002597809),
+        [3] = Color3.new(1, 1, 1),
+        [4] = Color3.new(0.27450981736183167, 0.27450981736183167, 0.27450981736183167)
+    },
+    [2] = "playerActionController"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Purchase"):InvokeServer(unpack(args))
+
+local args = {
+    [1] = {
+        [1] = "Audi-RSQ8",
+        [2] = Color3.new(0.7137255072593689, 0.38823530077934265, 0.062745101749897),
+        [3] = Color3.new(1, 1, 1),
+        [4] = Color3.new(0.8549019694328308, 0.8509804010391235, 0.8666666746139526)
+    },
+    [2] = "playerActionController"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Purchase"):InvokeServer(unpack(args))
+
+local args = {
+    [1] = {
+        [1] = "Audi-RS6Avant",
+        [2] = Color3.new(0.3803921639919281, 0.3960784375667572, 0.4156862795352936),
+        [3] = Color3.new(1, 1, 1),
+        [4] = Color3.new(0.7058823704719543, 0.7058823704719543, 0.7058823704719543)
+    },
+    [2] = "playerActionController"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Purchase"):InvokeServer(unpack(args))
+
+local args = {
+    [1] = "AudiCollections",
+    [2] = "AudiStartersI"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("AttemptClaimSubCollectionRewardRemoteFunction"):InvokeServer(unpack(args))
+
+
+local args = {
+    [1] = {
+        [1] = "Jaguar-XKR",
+        [2] = Color3.new(0.1568627506494522, 0.1568627506494522, 0.1568627506494522),
+        [3] = Color3.new(1, 1, 1),
+        [4] = Color3.new(0.5882353186607361, 0.5882353186607361, 0.5882353186607361)
+    },
+    [2] = "playerActionController"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Purchase"):InvokeServer(unpack(args))
+
+local args = {
+    [1] = {
+        [1] = "Jaguar-XKRSConvertible",
+        [2] = Color3.new(0.8745098114013672, 0.8745098114013672, 0.8705882430076599),
+        [3] = Color3.new(1, 1, 1),
+        [4] = Color3.new(0.9058823585510254, 0.9058823585510254, 0.9254902005195618)
+    },
+    [2] = "playerActionController"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Purchase"):InvokeServer(unpack(args))
+
+local args = {
+    [1] = {
+        [1] = "XJ220",
+        [2] = Color3.new(0.1725490242242813, 0.4117647111415863, 0.42352941632270813),
+        [3] = Color3.new(1, 1, 1),
+        [4] = Color3.new(0.6392157077789307, 0.6352941393852234, 0.6470588445663452)
+    },
+    [2] = "playerActionController"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Purchase"):InvokeServer(unpack(args))
+
+local args = {
+    [1] = "JaguarCollections",
+    [2] = "JaguarStartersI"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("AttemptClaimSubCollectionRewardRemoteFunction"):InvokeServer(unpack(args))
+
+local args = {
+    [1] = {
+        [1] = "McLarenMP4-12CSpider",
+        [2] = Color3.new(0.7843137383460999, 0.10588235408067703, 0.14901961386203766),
+        [3] = Color3.new(1, 1, 1),
+        [4] = Color3.new(0.37254902720451355, 0.37254902720451355, 0.37254902720451355)
+    },
+    [2] = "playerActionController"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Purchase"):InvokeServer(unpack(args))
+
+local args = {
+    [1] = {
+        [1] = "McLaren-570S",
+        [2] = Color3.new(0.06666667014360428, 0.06666667014360428, 0.06666667014360428),
+        [3] = Color3.new(1, 1, 1),
+        [4] = Color3.new(0.23529411852359772, 0.23529411852359772, 0.23529411852359772)
+    },
+    [2] = "playerActionController"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Purchase"):InvokeServer(unpack(args))
+
+local args = {
+    [1] = {
+        [1] = "McLaren-GT",
+        [2] = Color3.new(0.9098039269447327, 0.615686297416687, 0.47058823704719543),
+        [3] = Color3.new(1, 1, 1),
+        [4] = Color3.new(0.9058823585510254, 0.9058823585510254, 0.9254902005195618)
+    },
+    [2] = "playerActionController"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Purchase"):InvokeServer(unpack(args))
+
+local args = {
+    [1] = "McLarenCollections",
+    [2] = "TheMcLarenStartersI"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("AttemptClaimSubCollectionRewardRemoteFunction"):InvokeServer(unpack(args))
+
+
+
