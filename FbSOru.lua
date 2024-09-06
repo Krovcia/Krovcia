9a26e8ee63329e6b0e1b92ecab7be2340b51eccb
Krovcia <143706464+Krovcia@users.noreply.github.com>
2024-09-06 10:07:31 +0700

    Update FbSOru.lua

--- FbSOru.lua
+++ FbSOru.lua
@@ -18,14 +18,10 @@ local Button = Tab:CreateButton({
    Callback = function()
 while wait(1) do
 if game:GetService("Players").LocalPlayer.Character == nil and game:GetService("Players").LocalPlayer.PlayerGui.UI.HUD.Visible == false then
-    wait(1)
+wait(1)
 local VirtualInputManager = game:GetService("VirtualInputManager");
 VirtualInputManager:SendMouseButtonEvent(500, 380, 0, true, game, 1)
 VirtualInputManager:SendMouseButtonEvent(500, 380, 0, false, game, 1)
-wait(3)
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new( -1244, 696, -917, 0, 0, -1, 0, 1, 0, 1, 0, 0)
-wait(3)
-game:GetService("Players").LocalPlayer.Character.Humanoid.Health = 0
             else
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new( -1244, 696, -917, 0, 0, -1, 0, 1, 0, 1, 0, 0)
 wait(3)
