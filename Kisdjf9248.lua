b7f4ea3831557a9615a94029878b5c527f95835e
Krovcia <143706464+Krovcia@users.noreply.github.com>
2025-03-12 11:36:33 +0700

    Update Kisdjf9248.lua

--- Kisdjf9248.lua
+++ Kisdjf9248.lua
@@ -1,11 +1,13 @@
-
-
-
+while wait(600) do
 pcall(function()
-if not game:GetService("Players").LocalPlayer.PlayerGui and tick() - (LastNotif or 0) > 1 then
-                LastNotif = tick()
+  if not game:GetService("Players").LocalPlayer.Character and tick() - (LastNotif or 0) > 1 then
+                  LastNotif = tick()
       else
-      local args = {[1] = "CancelQuest",[2] = {}}
-      game:GetService("ReplicatedStorage"):WaitForChild("Chest"):WaitForChild("Remotes"):WaitForChild("Functions"):WaitForChild("EtcFunction"):InvokeServer(unpack(args))
-      gam
+        local args = {[1] = "CancelQuest",[2] = {}}
+        game:GetService("ReplicatedStorage"):WaitForChild("Chest"):WaitForChild("Remotes"):WaitForChild("Functions"):WaitForChild("EtcFunction"):InvokeServer(unpack(args))
+        game:GetService("Players").LocalPlayer.Character.Humanoid.Health = 0
+print("Death")
+  end
+end)
+end
 
