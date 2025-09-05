8d7951f6438cd2a946e00b8e0ae6aea9759fda3c
Krovcia <143706464+Krovcia@users.noreply.github.com>
2025-09-05 18:57:39 +0700

    Update KROVIAS.lua

--- KROVIAS.lua
+++ KROVIAS.lua
@@ -1,3 +1,13 @@
 NamaPengumpul = {"cielkuro55", "Cleohypen", "Chokocute99", "Fiosye"}
+TargetShroom = 100
+LimitTrade = 400000 
+
 HargaStallRev = 1100
 HargaStallMax = 800
+
+AttackOrder5 = {1200, 1200, 1200, 1200} 
+random5 = AttackOrder5[math.random(1,#AttackOrder5)]
+
+DeathPointTarget = random5
+
+MaxPlayerCount = 35
