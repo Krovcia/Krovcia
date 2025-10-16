cfc9bc1c979d2e6fac947226a8e4ff047d8379c8
Krovcia <143706464+Krovcia@users.noreply.github.com>
2025-10-16 18:24:36 +0700

    Update KROVIAS.lua

--- KROVIAS.lua
+++ KROVIAS.lua
@@ -5,7 +5,7 @@ LimitTrade = 400000
 
 HargaStallRev = 1100
 HargaStallMax = 800
-HargaDeathToken = 4000
+HargaDeathToken = 4500
 
 AttackOrder5 = {1200, 1200, 1200, 1200} 
 random5 = AttackOrder5[math.random(1,#AttackOrder5)]
