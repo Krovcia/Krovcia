7a9aa3c6c4df398bd8c7a556b4df6b71f2a81c2d
Krovcia <143706464+Krovcia@users.noreply.github.com>
2025-10-22 14:26:03 +0700

    Update KROVIAS.lua

--- KROVIAS.lua
+++ KROVIAS.lua
@@ -3,9 +3,9 @@ NamaPengumpul = {"Jeizvo", "Jeizva", "Hiejzo"}
 TargetShroom = 7000
 LimitTrade = 400000 
 
-HargaStallRev = 1200
-HargaStallMax = 900
-HargaDeathToken = 5000
+HargaStallRev = 1300
+HargaStallMax = 1000
+HargaDeathToken = 4300
 
 AttackOrder5 = {1200, 1200, 1200, 1200} 
 random5 = AttackOrder5[math.random(1,#AttackOrder5)]
