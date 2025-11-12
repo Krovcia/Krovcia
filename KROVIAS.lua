d729348f714dd33e23d927b47f90ad8f7eef0758
Krovcia <143706464+Krovcia@users.noreply.github.com>
2025-11-12 16:14:35 +0700

    Update KROVIAS.lua

--- KROVIAS.lua
+++ KROVIAS.lua
@@ -1,5 +1,4 @@
-NamaPengumpul = {"Prafvi", "Kyfiso", "Voefiz", "Kuzfio", "Veocpe"}
-
+NamaPengumpul = {"Besfiv", "Voefiz", "Eizevo"}
 
 TargetShroom = 7000
 LimitTrade = 500000
@@ -8,7 +7,7 @@ HargaStallRev = 1350
 HargaStallMax = 1050
 HargaDeathToken = 3500
 
-AttackOrder5 = {1200, 750, 750, 750, 750} 
+AttackOrder5 = {750, 750, 750, 750, 750} 
 random5 = AttackOrder5[math.random(1,#AttackOrder5)]
 
 DeathPointTarget = random5
