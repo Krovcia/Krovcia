9314beb9284a8aaa47ae07f52782f62eace647a7
Krovcia <143706464+Krovcia@users.noreply.github.com>
2025-11-16 16:09:55 +0700

    Update KROVIAS.lua

--- KROVIAS.lua
+++ KROVIAS.lua
@@ -10,7 +10,7 @@ HargaStallRev = 1350
 HargaStallMax = 1050
 HargaDeathToken = 3500
 
-AttackOrder5 = {750, 750, 750, 750, 750} 
+AttackOrder5 = {1250, 1250, 1250, 1250, 1250} 
 random5 = AttackOrder5[math.random(1,#AttackOrder5)]
 
 DeathPointTarget = random5
