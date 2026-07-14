fcfa7a816c68e01926ae7de428d3ccb27921ffbd
Krovcia <143706464+Krovcia@users.noreply.github.com>
2026-07-15 03:27:38 +0700

    Update KROVIAS.lua

--- KROVIAS.lua
+++ KROVIAS.lua
@@ -1,13 +1,13 @@
 _G.NamaPengumpul = {"Joecve", "Micvja", "3", "4", "5", "6"}
 _G.baseUrl = "https://script.google.com/macros/s/AKfycbwTs23_OVHs5cJ9xXDjgrZwZWlyzDGnoDwwt6fElnOwnlxK2povKxB5b7hc-qeVvZsu/exec"
 
-_G.TargetMush = 7000
+_G.TargetMush = 10000
 _G.DeathPointTarget = 1200
 
 local ShadowPriceTarget = 1700
 local WardenPriceTarget = 1000
-_G.ShadowTarget = 5
-_G.WardenTarget = 10
+_G.ShadowTarget = 2
+_G.WardenTarget = 5
 
 _G.Artifacttobuy = {
     {Name = "Akanthos",   Type = "Shadow"},
@@ -28,38 +28,40 @@ _G.Artifacttobuy = {
 
 _G.RestockCD = 10 
 _G.MySlots = {
-    {NameItem = "CreatureReviveToken", ItemType = "Items", Harga = 1400, MaxStok = 50},
-    {NameItem = "FullGrowToken", ItemType = "Items", Harga = 900, MaxStok = 50},
-    {NameItem = "DeathGachaToken", ItemType = "Items", Harga = 7000, MaxStok = 20},
-
-    {NameItem = "Khilutatra", ItemType = "Unlocks", Harga = 100000, MaxStok = 3},
-    {NameItem = "Zakaiodrak", ItemType = "Unlocks", Harga = 100000, MaxStok = 3},
-    {NameItem = "Veishyadar", ItemType = "Unlocks", Harga = 100000, MaxStok = 3},
+    {NameItem = "Khilutatra", ItemType = "Unlocks", Harga = 85000, MaxStok = 3},
+    {NameItem = "Zakaiodrak", ItemType = "Unlocks", Harga = 85000, MaxStok = 3},
+    {NameItem = "Veishyadar", ItemType = "Unlocks", Harga = 85000, MaxStok = 3},
     {NameItem = "Xymorax", ItemType = "Unlocks", Harga = 850000, MaxStok = 3},
     {NameItem = "Zokzis", ItemType = "Unlocks", Harga = 85000, MaxStok = 3},
     {NameItem = "Pentagloss", ItemType = "Unlocks", Harga = 85000, MaxStok = 3},
+    {NameItem = "Thryxx", ItemType = "Unlocks", Harga = 100000, MaxStok = 3},
+    
+    {NameItem = "Dabaemura", ItemType = "Unlocks", Harga = 25000, MaxStok = 5},
+    {NameItem = "Ghartokus", ItemType = "Unlocks", Harga = 25000, MaxStok = 5},
+    {NameItem = "Morthorax", ItemType = "Unlocks", Harga = 25000, MaxStok = 5},
+    {NameItem = "Phantaris", ItemType = "Unlocks", Harga = 25000, MaxStok = 5},
+    {NameItem = "Prialoura", ItemType = "Unlocks", Harga = 25000, MaxStok = 5},
 
-    {NameItem = "Dabaemura", ItemType = "Unlocks", Harga = 28000, MaxStok = 5},
-    {NameItem = "Ghartokus", ItemType = "Unlocks", Harga = 28000, MaxStok = 5},
-    {NameItem = "Morthorax", ItemType = "Unlocks", Harga = 28000, MaxStok = 5},
-    {NameItem = "Phantaris", ItemType = "Unlocks", Harga = 28000, MaxStok = 5},
-    {NameItem = "Prialoura", ItemType = "Unlocks", Harga = 28000, MaxStok = 5},
+    {NameItem = "CreatureReviveToken", ItemType = "Items", Harga = 1400, MaxStok = 50},
+    {NameItem = "FullGrowToken", ItemType = "Items", Harga = 900, MaxStok = 50},
+    {NameItem = "DeathGachaToken", ItemType = "Items", Harga = 6000, MaxStok = 20},
 }
 
 _G.TargetStall = {
     {Name = "FullGrowToken", MaxPrice = 100, MaxHave = 10, ItemType = "Items", Type = "Selling"},
     {Name = "CreatureReviveToken", MaxPrice = 100, MaxHave = 10, ItemType = "Items", Type = "Selling"},
 
-    {Name = "Khilutatra", MinPrice = 100000, MaxHave = 3, ItemType = "Unlocks", Type = "Buying"},
-    {Name = "Zakaiodrak", MinPrice = 100000, MaxHave = 3, ItemType = "Unlocks", Type = "Buying"},
-    {Name = "Veishyadar", MinPrice = 100000, MaxHave = 3, ItemType = "Unlocks", Type = "Buying"},
+    {Name = "Khilutatra", MinPrice = 850000, MaxHave = 3, ItemType = "Unlocks", Type = "Buying"},
+    {Name = "Zakaiodrak", MinPrice = 850000, MaxHave = 3, ItemType = "Unlocks", Type = "Buying"},
+    {Name = "Veishyadar", MinPrice = 85000, MaxHave = 3, ItemType = "Unlocks", Type = "Buying"},
     {Name = "Xymorax", MinPrice = 850000, MaxHave = 3, ItemType = "Unlocks", Type = "Buying"},
     {Name = "Zokzis", MinPrice = 85000, MaxHave = 3, ItemType = "Unlocks", Type = "Buying"},
     {Name = "Pentagloss", MinPrice = 85000, MaxHave = 3, ItemType = "Unlocks", Type = "Buying"},
-
-    {Name = "Dabaemura", MinPrice = 28000, MaxHave = 5, ItemType = "Unlocks", Type = "Buying"},
-    {Name = "Ghartokus", MinPrice = 28000, MaxHave = 5, ItemType = "Unlocks", Type = "Buying"},
-    {Name = "Morthorax", MinPrice = 28000, MaxHave = 5, ItemType = "Unlocks", Type = "Buying"},
-    {Name = "Phantaris", MinPrice = 28000, MaxHave = 5, ItemType = "Unlocks", Type = "Buying"},
-    {Name = "Prialoura", MinPrice = 28000, MaxHave = 5, ItemType = "Unlocks", Type = "Buying"},
+    {Name = "Thryxx", MinPrice = 100000, MaxHave = 3, ItemType = "Unlocks", Type = "Buying"},
+    
+    {Name = "Dabaemura", MinPrice = 25000, MaxHave = 5, ItemType = "Unlocks", Type = "Buying"},
+    {Name = "Ghartokus", MinPrice = 25000, MaxHave = 5, ItemType = "Unlocks", Type = "Buying"},
+    {Name = "Morthorax", MinPrice = 25000, MaxHave = 5, ItemType = "Unlocks", Type = "Buying"},
+    {Name = "Phantaris", MinPrice = 25000, MaxHave = 5, ItemType = "Unlocks", Type = "Buying"},
+    {Name = "Prialoura", MinPrice = 25000, MaxHave = 5, ItemType = "Unlocks", Type = "Buying"},
 }
