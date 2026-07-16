d9585913455325532121376d3d5a715f58e289e1
Krovcia <143706464+Krovcia@users.noreply.github.com>
2026-07-17 02:21:42 +0700

    Update KROVIAS.lua

--- KROVIAS.lua
+++ KROVIAS.lua
@@ -1,7 +1,7 @@
 _G.NamaPengumpul = {"Zyfiao", "Lozsce ", "3", "4", "5", "6"}
 _G.baseUrl = "https://script.google.com/macros/s/AKfycbwTs23_OVHs5cJ9xXDjgrZwZWlyzDGnoDwwt6fElnOwnlxK2povKxB5b7hc-qeVvZsu/exec"
 
-_G.TargetMush = 10000
+_G.TargetMush = 100000
 _G.DeathPointTarget = 1200
 
 local ShadowPriceTarget = 1700
