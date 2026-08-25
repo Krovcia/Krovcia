f8714a6938804009acabe46421b3f6024f1d0c1c
Krovcia <143706464+Krovcia@users.noreply.github.com>
2026-08-25 12:38:20 +0700

    Update KROVIAS.lua

--- KROVIAS.lua
+++ KROVIAS.lua
@@ -1,5 +1,6 @@
-_G.NamaPengumpul = {"Lozsce", "Micvja", "3", "4", "5", "6"}
+_G.NamaPengumpul = {"Joecve", "Zyfiao", "3", "4", "5", "6"}
  
+
 _G.baseUrl = "https://script.google.com/macros/s/AKfycbwTs23_OVHs5cJ9xXDjgrZwZWlyzDGnoDwwt6fElnOwnlxK2povKxB5b7hc-qeVvZsu/exec"
 
 _G.TargetMush = 1000000
